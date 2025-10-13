0321e7120b5b5037b4022a9cbb621e101dad4e10
wirecoreltd <wirecoreltd@gmail.com>
2025-10-13 15:52:40 +0400

    Update home.js

--- pages/home.js
+++ pages/home.js
@@ -6,7 +6,7 @@ import Link from "next/link";
 import Image from "next/image";
 import { useRouter } from "next/router";
 import supabase from "../lib/supabaseClient";
-import SendAppLinkPopup from "../components/SendAppLinkPopup"; // Nouveau membre
+import SendLinkPopup from "../components/SendAppLinkPopup"; // Nouveau membre
 import SendAppLinkEvangelise from "../components/SendAppLinkEvangelise"; // Évangélisé
 
 export default function Home() {

3cc04793eb4eed2a8b856988ec948cbca0c4587c
wirecoreltd <wirecoreltd@gmail.com>
2025-10-17 08:11:58 +0400

    Update create-internal-user.js

--- pages/admin/create-internal-user.js
+++ pages/admin/create-internal-user.js
@@ -25,7 +25,7 @@ export default function CreateInternalUserPage() {
     nom: "",
     email: "",
     password: "",
-    role: "Membre",
+    role: "",
   });
 
   // 🧩 Vérification d’accès
@@ -184,6 +184,7 @@ export default function CreateInternalUserPage() {
             className="w-full border rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-400"
           >
             <option value="ResponsableIntegration">Responsable Intégration</option>
+<option value="ResponsableCellule">Responsable Cellule</option>
             <option value="ResponsableEvangelisation">Responsable Évangélisation</option>
             <option value="Membre">Membre</option>
           </select>

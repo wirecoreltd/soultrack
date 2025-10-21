70a9aa27004e6b77c57452321c5daa2491524e5b
wirecoreltd <wirecoreltd@gmail.com>
2025-10-21 15:40:56 +0400

    Update cellules-hub.js

--- pages/cellules-hub.js
+++ pages/cellules-hub.js
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
-import { supabase } from "@/lib/supabaseClient";
+import supabase from "../lib/supabaseClient";
 
 export default function CelluleHub() {
   const router = useRouter();

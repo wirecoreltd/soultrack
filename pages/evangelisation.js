6808767af688358491598e4c654d1fba5176a61e
wirecoreltd <wirecoreltd@gmail.com>
2025-10-24 16:09:05 +0400

    Update evangelisation.js

--- pages/evangelisation.js
+++ pages/evangelisation.js
@@ -4,6 +4,7 @@
 import { useEffect, useState } from "react";
 import supabase from "../lib/supabaseClient";
 import Image from "next/image";
+import AccessGuard from "../components/AccessGuard";
 
 export default function Evangelisation() {
   const [contacts, setContacts] = useState([]);

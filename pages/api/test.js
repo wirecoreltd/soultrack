61ce90f2dfdf9a4cf2aa5447cfa0f9be45fc610e
wirecoreltd <wirecoreltd@gmail.com>
2025-10-29 16:13:44 +0400

    Update test.js

--- pages/api/test.js
+++ pages/api/test.js
@@ -1,4 +1,4 @@
-// /pages/api/test.js
 export default function handler(req, res) {
+  if (req.method !== "POST") return res.status(405).json({ error: "Méthode non autorisée" });
   res.status(200).json({ ok: true });
 }

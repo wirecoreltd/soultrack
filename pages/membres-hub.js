81a9360907a5c9b016e3380558fba3a687293aef
wirecoreltd <wirecoreltd@gmail.com>
2025-11-17 12:05:24 +0400

    Update membres-hub.js

--- pages/membres-hub.js
+++ pages/membres-hub.js
@@ -94,6 +94,17 @@ export default function MembresHub() {
         </Link>
       </div>
 
+      <Link
+          href="/pages/create-conseiller"
+          className="flex-1 bg-white rounded-2xl shadow-md flex flex-col justify-center items-center border-t-4 border-[#FBBF24] p-6 hover:shadow-lg transition-all duration-200 cursor-pointer h-32"
+        >
+          <div className="text-4xl mb-2">➕</div>
+          <div className="text-lg font-bold text-gray-800 text-center">
+            Suivis des membres
+          </div>
+        </Link>
+      </div>
+
       {/* 🔹 Bouton popup ajouté sous les cartes */}
       <div className="w-full max-w-md mb-10">
         <SendLinkPopup

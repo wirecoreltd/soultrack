7a007f4246f69d1424556df9faca573b6c9d6133
wirecoreltd <wirecoreltd@gmail.com>
2025-11-17 10:03:51 +0400

    Update list-members.js

--- pages/list-members.js
+++ pages/list-members.js
@@ -97,7 +97,7 @@ export default function ListMembers() {
   const getBorderColor = (m) => {
     if (m.star) return "#FBC02D";
     if (m.statut === "actif") return "#4285F4";
-    if (m.statut === "a déjà son église") return "#EA4335";
+    if (m.statut === "a déjà son église") return "#f21705";
     if (m.statut === "integrer") return "#FFA500";
     if (m.statut === "ancien") return "#999999";
     if (m.statut === "veut rejoindre ICC" || m.statut === "visiteur") return "#34A853";

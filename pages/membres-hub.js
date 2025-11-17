b42f288c49cb861b76888d6cc0ad3da6edcdbf52
wirecoreltd <wirecoreltd@gmail.com>
2025-11-17 12:08:41 +0400

    Update membres-hub.js

--- pages/membres-hub.js
+++ pages/membres-hub.js
@@ -95,12 +95,12 @@ export default function MembresHub() {
       </div>
 
       <Link
-          href="/pages/create-conseiller"
+          href="/create-conseiller"
           className="flex-1 bg-white rounded-2xl shadow-md flex flex-col justify-center items-center border-t-4 border-[#FBBF24] p-6 hover:shadow-lg transition-all duration-200 cursor-pointer h-32"
         >
           <div className="text-4xl mb-2">➕</div>
           <div className="text-lg font-bold text-gray-800 text-center">
-            Suivis des membres
+            Crée un Conseiller
           </div>
         </Link>
       </div>

cc3d8d17e91c468e8d8a3849c30bcef6962280f9
wirecoreltd <wirecoreltd@gmail.com>
2025-11-21 13:33:13 +0400

    Update membres-hub.js

--- pages/membres-hub.js
+++ pages/membres-hub.js
@@ -57,17 +57,7 @@ export default function MembresHub() {
         </p>
       </div>
 
-      {/* 🔹 Cartes principales */}
-      <div className="flex flex-col md:flex-row gap-6 justify-center w-full max-w-5xl mb-6">
-        <Link
-          href="/add-member"
-          className="flex-1 bg-white rounded-2xl shadow-md flex flex-col justify-center items-center border-t-4 border-[#0EA5E9] p-6 hover:shadow-lg transition-all duration-200 cursor-pointer h-32"
-        >
-          <div className="text-4xl mb-2">➕</div>
-          <div className="text-lg font-bold text-gray-800 text-center">
-            Ajouter un membre
-          </div>
-        </Link>
+      {/* 🔹 Cartes principales */}    
 
         <Link
           href="/list-members"

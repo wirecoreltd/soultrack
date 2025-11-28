d9591b6f40c6738737d5409f52dca9ef1dc4ab7b
wirecoreltd <wirecoreltd@gmail.com>
2025-11-28 05:02:01 +0400

    Update list-members.js

--- pages/list-members.js
+++ pages/list-members.js
@@ -415,7 +415,7 @@ export default function ListMembers() {
         >
           {isOpen ? "Fermer détails" : "Détails"}
         </button>
-      </div>
+      
 
       {/* Détails ouverts */}
       {isOpen && (

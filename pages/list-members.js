5ab5783e5606dd27bfebad9e0d908b1121594f24
wirecoreltd <wirecoreltd@gmail.com>
2025-12-17 14:43:09 +0400

    Update list-members.js

--- pages/list-members.js
+++ pages/list-members.js
@@ -412,8 +412,6 @@ export default function ListMembers() {
       {showingToast && (
         <div className="fixed bottom-6 right-6 bg-green-500 text-white px-4 py-2 rounded shadow-lg">
           {toastMessage}
-        </div>
-      )}
-    </div>
+        </div>     
   );
 }

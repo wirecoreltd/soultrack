d494beafcbc0b60c5b0113b98a763fe93c187131
wirecoreltd <wirecoreltd@gmail.com>
2025-12-19 09:37:52 +0400

    Update EditMemberPopup.js

--- components/EditMemberPopup.js
+++ components/EditMemberPopup.js
@@ -226,8 +226,7 @@ export default function EditMemberPopup({ member, onClose, onUpdateMember }) {
             <select name="statut" value={formData.statut} onChange={handleChange} className="input">
               <option value="">-- Statut --</option>
               <option value="actif">Actif</option>
-              <option value="a déjà son église">A déjà son église</option>
-              <option value="integrer">Intégrer</option>
+              <option value="a déjà son église">A déjà son église</option>              
               <option value="ancien">Ancien</option>
             
             </select>

1992f88df2720892c59499f7065cb679ca435fa1
wirecoreltd <wirecoreltd@gmail.com>
2025-12-22 11:38:44 +0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import "../styles/globals.css";
+import { MembersProvider } from "../context/MembersContext";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <MembersProvider>
+      <Component {...pageProps} />
+    </MembersProvider>
+  );
 }
 
 export default MyApp;

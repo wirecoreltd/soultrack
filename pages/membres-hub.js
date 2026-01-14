69e5193a7d751f2994c30e9d60a1fb7d438c3edf
wirecoreltd <wirecoreltd@gmail.com>
2026-01-14 19:17:40 +0400

    Update membres-hub.js

--- pages/membres-hub.js
+++ pages/membres-hub.js
@@ -6,7 +6,7 @@ import LogoutLink from "../components/LogoutLink";
 import SendLinkPopup from "../components/SendLinkPopup";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
-import Header from "../components/Header";
+import Header from "../components/HeaderPges";
 
 export default function MembresHub() {
   const router = useRouter();

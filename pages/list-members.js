8f22c697f15cad2081db9a14812aed3e285799e6
wirecoreltd <wirecoreltd@gmail.com>
2026-01-19 17:05:29 +0400

    Update list-members.js

--- pages/list-members.js
+++ pages/list-members.js
@@ -275,7 +275,7 @@ export default function ListMembers() {
         <div key={m.id} className="bg-white p-3 rounded-xl shadow-md border-l-4 relative">
           {/* Badge Nouveau en haut à droite */}
           {m.isNouveau && (
-            <div className="absolute top+2 right-3 flex items-center gap-1">
+            <div className="absolute top-6 right-3 flex items-center gap-1">
               <div className="absolute top-0 right-2 flex items-center gap-1">  
               <span className="w-3 h-3 rounded-full" style={{ backgroundColor: "#2E3192" }}></span>  
               <span className="text-xs font-semibold" style={{ color: "#2E3192" }}>Nouveau</span>

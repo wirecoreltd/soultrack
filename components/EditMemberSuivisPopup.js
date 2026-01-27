3b862c30712d79803f421814968ed0f12a562d72
wirecoreltd <wirecoreltd@gmail.com>
2026-01-27 22:26:02 +0400

    Update EditMemberSuivisPopup.js

--- components/EditMemberSuivisPopup.js
+++ components/EditMemberSuivisPopup.js
@@ -264,8 +264,8 @@ export default function EditMemberPopup({ member, onClose, onUpdateMember }) {
             <label className="font-medium">Bapteme d'eau</label>
             <select name="bapteme_eau" value={formData.bapteme_eau === true ? "true" : formData.bapteme_eau === false ? "false" : ""} onChange={handleChange} className="input">
               <option value="">-- Sélectionner --</option>
-              <option value="true">Oui</option>
-              <option value="false">Non</option>
+              <option value="Oui">Oui</option>
+              <option value="Non">Non</option>
             </select>
           </div>
 
@@ -274,8 +274,8 @@ export default function EditMemberPopup({ member, onClose, onUpdateMember }) {
             <label className="font-medium">Bapteme de feu</label>
             <select name="bapteme_esprit" value={formData.bapteme_esprit === true ? "true" : formData.bapteme_esprit === false ? "false" : ""} onChange={handleChange} className="input">
               <option value="">-- Sélectionner --</option>
-              <option value="true">Oui</option>
-              <option value="false">Non</option>
+              <option value="Oui">Oui</option>
+              <option value="Non">Non</option>
             </select>
           </div>
 

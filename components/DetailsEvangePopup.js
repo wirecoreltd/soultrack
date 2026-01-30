fb6f903a56dcb8a505710e2b5def06783a4d8f21
wirecoreltd <wirecoreltd@gmail.com>
2026-01-30 13:57:25 +0400

    Update DetailsEvangePopup.js

--- components/DetailsEvangePopup.js
+++ components/DetailsEvangePopup.js
@@ -189,7 +189,7 @@ export default function DetailsEvangePopup({ member, onClose, onEdit }) {
         <div className="mt-4 rounded-xl w-full shadow-md p-4 bg-white">
           <button
             onClick={() => onEdit(member)}
-            className="w-full py-2 rounded-md bg-white text-orange-500 shadow-md"
+            className="w-full py-2 rounded-md bg-white text-orange-500"
           >
             ✏️ Modifier le contact
           </button>

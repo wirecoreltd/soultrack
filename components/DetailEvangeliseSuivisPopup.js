9b02ba2b723b520eb4edb97b91a3f4f5a52c9156
wirecoreltd <wirecoreltd@gmail.com>
2026-01-30 08:46:41 +0400

    Update DetailEvangeliseSuivisPopup.js

--- components/DetailEvangeliseSuivisPopup.js
+++ components/DetailEvangeliseSuivisPopup.js
@@ -309,7 +309,6 @@ export default function DetailEvangeliseSuivisPopup({
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/50">
       <div className="bg-white rounded-lg p-6 w-full max-w-lg">
 
-        {/* ================= CENTRÉ ================= */}
         {!isRefus && (
           <div className="mt-6 flex justify-center">
             <button

41dda15c891bb07db2559115253edc5353089166
wirecoreltd <wirecoreltd@gmail.com>
2026-01-30 12:37:41 +0400

    Update suivis-evangelisation.js

--- pages/suivis-evangelisation.js
+++ pages/suivis-evangelisation.js
@@ -315,7 +315,7 @@ export default function SuivisEvangelisation() {
 
   return (
     
-  <div className="min-h-screen flex flex-col items-center p-6 bg-gradient-to-r from-blue-800 to-cyan-400">
+  <div className="min-h-screen flex flex-col items-center p-6 bg-[#333699]">
     
     <HeaderPages />
     

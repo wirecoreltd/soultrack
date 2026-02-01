746a6c590c4b92a26e7be4b02471a87410a4ed3e
wirecoreltd <wirecoreltd@gmail.com>
2026-02-01 11:46:36 +0400

    Update cellules-hub.js

--- pages/cellules-hub.js
+++ pages/cellules-hub.js
@@ -47,19 +47,7 @@ export default function CellulesHub() {
       >
         <HeaderPages />
         
-        {/* 🔹 Cartes principales */}
-         <div className="flex flex-col md:flex-row gap-6 justify-center w-full max-w-5xl mb-6 flex-wrap">
-          <Link
-            href="/admin/list-cellules"
-            className="flex-1 min-w-[250px] w-full h-32 bg-white rounded-3xl shadow-md flex flex-col justify-center items-center border-t-4 border-blue-500 p-6 hover:shadow-xl transition-all duration-200 cursor-pointer"
-          >
-            <div className="text-5xl mb-2">➕</div>
-            <div className="text-lg font-bold text-gray-800 text-center">
-              Ajouter une Cellule
-            </div>
-          </Link>  
-
-        {/* Liste des Cellules */}
+       {/* Liste des Cellules */}
 
         <div className="flex flex-col md:flex-row gap-6 justify-center w-full max-w-5xl mb-6 flex-wrap">
           <Link

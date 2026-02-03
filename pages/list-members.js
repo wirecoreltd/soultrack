d72db3c113235803e1e4a9f434bc458d5323d549
wirecoreltd <wirecoreltd@gmail.com>
2026-02-03 13:03:08 +0400

    Update list-members.js

--- pages/list-members.js
+++ pages/list-members.js
@@ -13,17 +13,8 @@ import { useSearchParams } from "next/navigation";
 import { useMembers } from "../context/MembersContext";
 import Header from "../components/Header";
 import { useRouter } from "next/navigation";
-import ProtectedRoute from "../components/ProtectedRoute";
-import ListMembersContent from "../components/ListMembersContent";
 
-export default function ListMembersPage() {
-  return (
-    <ProtectedRoute allowedRoles={["Administrateur", "Conseiller", "ResponsableCellule"]}>
-      <ListMembersContent />
-    </ProtectedRoute>
-  );
-}
-
+export default function ListMembers() {
   const [filter, setFilter] = useState("");
   const [search, setSearch] = useState("");
   const [detailsOpen, setDetailsOpen] = useState({});

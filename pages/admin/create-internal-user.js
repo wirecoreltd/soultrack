8ef1b6bd7d3bd673f37abe424ca6fd274b32fb19
wirecoreltd <wirecoreltd@gmail.com>
2026-02-05 11:10:45 +0400

    Update create-internal-user.js

--- pages/admin/create-internal-user.js
+++ pages/admin/create-internal-user.js
@@ -2,7 +2,7 @@
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 import Image from "next/image";
-import supabase from "../lib/supabaseClient";
+import supabase from "../../lib/supabaseClient";
 
 export default function CreateInternalUser({ egliseId, brancheId }) {
   const router = useRouter();

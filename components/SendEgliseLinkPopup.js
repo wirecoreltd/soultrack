6ef286f32699324d40ffae4a9ffaffede1c6628d
wirecoreltd <wirecoreltd@gmail.com>
2026-02-09 12:59:37 +0400

    Update SendEgliseLinkPopup.js

--- components/SendEgliseLinkPopup.js
+++ components/SendEgliseLinkPopup.js
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import supabase from "../../lib/supabaseClient";
+import supabase from "../lib/supabaseClient";
 import { v4 as uuidv4 } from "uuid";
 
 export default function SendEgliseLinkPopup({ label, type, buttonColor, eglise, superviseur }) {

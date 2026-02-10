bfbfba459d1c83b69e9ac980d17c0dab221fa0e5
wirecoreltd <wirecoreltd@gmail.com>
2026-02-10 16:11:39 +0400

    Update link-eglise.js

--- pages/admin/link-eglise.js
+++ pages/admin/link-eglise.js
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import supabase from "../../lib/supabaseClient";
+import supabase from "../lib/supabaseClient";
 import SendEgliseLinkPopup from "../../components/SendEgliseLinkPopup";
 import HeaderPages from "../../components/HeaderPages";
 

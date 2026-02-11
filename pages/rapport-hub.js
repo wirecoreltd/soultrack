92428fdde19d142df46fa90f668f9ef20e04c188
wirecoreltd <wirecoreltd@gmail.com>
2026-02-11 14:18:28 +0400

    Update rapport-hub.js

--- pages/rapport-hub.js
+++ pages/rapport-hub.js
@@ -91,7 +91,7 @@ export default function RapportHub() {
 
         {/* Statistiques globales */}
         <Link
-          href="/rapport/statistiques-globales"
+          href="/StatGlobalPage"
           className="flex-1 bg-white rounded-2xl shadow-md flex flex-col justify-center items-center 
           border-t-4 border-[#10B981] p-6 hover:shadow-lg transition-all duration-200 cursor-pointer h-32"
         >

a4854c97e2fc54adbf311e8c9f9d1477a67058be
wirecoreltd <wirecoreltd@gmail.com>
2026-02-11 13:59:48 +0400

    Update link-eglise.js

--- pages/admin/link-eglise.js
+++ pages/admin/link-eglise.js
@@ -176,7 +176,7 @@ export default function LinkEglise() {
       <div className="h-10" />
 
       {/* TITRE TABLE */}
-      <h4 className="text-2xl font-bold mt-3 mb-2 text-center w-full max-w-5xl">
+      <h4 className="text-2xl font-bold mt-3 mb-8 text-center w-full max-w-5xl">
         Liste des églises supervisées
       </h4>
 

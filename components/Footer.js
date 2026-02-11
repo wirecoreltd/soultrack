2055dc2d40769514d039925fb48bad0255ebb8f2
wirecoreltd <wirecoreltd@gmail.com>
2026-02-11 10:52:39 +0400

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -5,7 +5,7 @@ export default function Footer() {
     <footer className="w-full py-4 mt-auto">
       <div
         className="max-w-5xl mx-auto text-center text-sm"
-        style={{ color: "#F8F8F6" }}
+        style={{ color: "#808080" }}
       >
         © {new Date().getFullYear()} SoulTrack. Tous droits réservés.
       </div>

4bf7b3650030526ee9d7d3c9b923ce877abaf05c
wirecoreltd <wirecoreltd@gmail.com>
2026-02-12 09:57:15 +0400

    Update cellules-hub.js

--- pages/cellules-hub.js
+++ pages/cellules-hub.js
@@ -12,6 +12,7 @@ import { useEffect, useState } from "react";
 import supabase from "../lib/supabaseClient";
 import HeaderPages from "../components/HeaderPages";
 import ProtectedRoute from "../components/ProtectedRoute"; 
+import Footer from "../components/Footer";
 
 export default function CellulesHub() {
   return (

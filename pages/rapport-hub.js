bf0218b78e3224ecc5e7af521a13ab6aad74dc7e
wirecoreltd <wirecoreltd@gmail.com>
2026-02-12 16:36:19 +0400

    Update rapport-hub.js

--- pages/rapport-hub.js
+++ pages/rapport-hub.js
@@ -95,7 +95,7 @@ export default function RapportHub() {
           className="flex-1 bg-white rounded-2xl shadow-md flex flex-col justify-center items-center 
           border-t-4 border-[#3B82F6] p-6 hover:shadow-lg transition-all duration-200 cursor-pointer h-32"
         >
-          <div className="text-4xl mb-2">💧</div>
+          <div className="text-4xl mb-2">✒️</div>
           <div className="text-lg font-bold text-gray-800 text-center">Rapport Formation</div>
         </Link>
 

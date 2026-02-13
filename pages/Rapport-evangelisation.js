aec4784e78d26925e69e1e7f2c40f0eca6de5501
wirecoreltd <wirecoreltd@gmail.com>
2026-02-13 17:16:47 +0400

    Update Rapport-evangelisation.js

--- pages/Rapport-evangelisation.js
+++ pages/Rapport-evangelisation.js
@@ -204,7 +204,7 @@ export default function RapportEvangelisation() {
                 {rapports.reduce((s, r) => s + Number(r.femmes || 0), 0)}
               </div>
             
-              <div className="min-w-[120px] text-center text-white font-bold ml-1">
+              <div className="min-w-[120px] text-center text-white font-bold ml-0.5">
                 {rapports.reduce(
                   (s, r) => s + Number(r.hommes || 0) + Number(r.femmes || 0),
                   0

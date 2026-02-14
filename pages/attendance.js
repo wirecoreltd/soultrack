f1dccfc534f9b9a1a09f22e74a483cdac85a06d8
wirecoreltd <wirecoreltd@gmail.com>
2026-02-14 14:35:54 +0400

    Update attendance.js

--- pages/attendance.js
+++ pages/attendance.js
@@ -294,8 +294,9 @@ const totalPrincipal =
                   <button onClick={() => handleEdit(r)} className="text-blue-400 hover:text-blue-600">✏️</button>
                   <button onClick={() => handleDelete(r.id)} className="text-red-400 hover:text-red-600">🗑️</button>           
 
-                </div>
-            {/* 🔹 TOTAL BAS */}
+                </div>            
+              </div>
+         {/* 🔹 TOTAL BAS */}
 <div className="flex items-center px-4 py-4 mt-2 rounded-xl bg-white/20 border-t border-white/40 font-bold">
   <div className="min-w-[150px] text-white uppercase">TOTAL</div>
   <div className="min-w-[120px] text-center text-white">{totalGlobal.hommes}</div>
@@ -308,7 +309,6 @@ const totalPrincipal =
   <div className="min-w-[180px] text-center text-white">{totalGlobal.nouveauxConvertis}</div>
   <div className="min-w-[140px]"></div>
 </div>
-              </div>
             );
           })}
         </div>

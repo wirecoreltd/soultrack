a57f96550f11d0d01ba42aba0afb732f52162202
wirecoreltd <wirecoreltd@gmail.com>
2026-02-16 20:19:26 +0400

    Update attendance.js

--- pages/attendance.js
+++ pages/attendance.js
@@ -394,18 +394,17 @@ function Attendance() {
 </div>
 
 
+<Footer />
 
-      <Footer />
-
-      <style jsx>{`
-        .input {
-          width: 100%;
-          border: 1px solid #ccc;
-          border-radius: 12px;
-          padding: 10px;
-          box-shadow: 0 1px 2px rgba(0,0,0,0.1);
-        }
-      `}</style>
-    </div>
-  );
+<style jsx>{`
+  .input {
+    width: 100%;
+    border: 1px solid #ccc;
+    border-radius: 12px;
+    padding: 10px;
+    box-shadow: 0 1px 2px rgba(0,0,0,0.1);
+  }
+`}</style>
+</div>
+);
 }

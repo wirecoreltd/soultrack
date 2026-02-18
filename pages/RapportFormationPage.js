2bb3c9138df45e0f0b37d033a935c79f7d06d7de
wirecoreltd <wirecoreltd@gmail.com>
2026-02-18 16:49:57 +0400

    Update RapportFormationPage.js

--- pages/RapportFormationPage.js
+++ pages/RapportFormationPage.js
@@ -224,7 +224,7 @@ function RapportFormation() {
         <div className="min-w-[180px] text-white font-semibold">
           {isExpanded ? "➖" : "➕"} {monthLabel}
         </div>
-        <div className="min-w-[120px] text-center text-white font-bold ml-40">{totalMonth.hommes}</div>
+        <div className="min-w-[120px] text-center text-white font-bold ml-70">{totalMonth.hommes}</div>
         <div className="min-w-[120px] text-center text-white font-bold">{totalMonth.femmes}</div>
         <div className="min-w-[120px] text-center text-orange-500 font-semibold ml-20">{totalMonth.total}</div>
         <div className="min-w-[200px]"></div>

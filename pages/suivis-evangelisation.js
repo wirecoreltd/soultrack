aedf95d5171b954ef6694810ddc0eae0466642af
wirecoreltd <wirecoreltd@gmail.com>
2026-02-20 14:03:20 +0400

    Update suivis-evangelisation.js

--- pages/suivis-evangelisation.js
+++ pages/suivis-evangelisation.js
@@ -12,7 +12,7 @@ import Footer from "../components/Footer";
 
 export default function SuivisEvangelisation() {
   return (
-    <ProtectedRoute allowedRoles={["Administrateur", "ResponsableEvangelisation", "Conseiller"]}>
+    <ProtectedRoute allowedRoles={["Administrateur", "ResponsableEvangelisation", "ResponsableCellule", "Conseiller"]}>
       <SuivisEvangelisationContent />
     </ProtectedRoute>
   );

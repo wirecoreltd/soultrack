99af1d62ccf9cbd69a677be52c64035bded12a68
wirecoreltd <wirecoreltd@gmail.com>
2026-02-23 18:56:16 +0400

    Update Rapport-evangelisation.js

--- pages/Rapport-evangelisation.js
+++ pages/Rapport-evangelisation.js
@@ -109,8 +109,10 @@ export default function RapportEvangelisation() {
     <div className="min-h-screen flex flex-col items-center p-6 bg-[#333699]">
       <HeaderPages />
 
-      <h1 className="text-2xl font-bold text-white mt-4">Rapport Évangélisation</h1>
-
+            <h1 className="text-2xl font-bold mt-4 mb-6 text-center">
+        <span className="text-white">Rapport </span>
+        <span className="text-amber-300">Evangélisation</span>
+      </h1>
       {/* FILTRES */}
       <div className="bg-white/10 p-6 rounded-2xl shadow-lg mt-6 flex justify-center gap-4 flex-wrap text-white">
         <div className="flex flex-col w-full sm:w-auto">

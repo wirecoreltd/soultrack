be1a1006ac88bc277f7549916353a60b332bd250
wirecoreltd <wirecoreltd@gmail.com>
2026-03-06 11:19:21 +0400

    Update EditMemberPopup.js

--- components/EditMemberPopup.js
+++ components/EditMemberPopup.js
@@ -59,6 +59,7 @@ export default function EditMemberPopup({ member, onClose, onUpdateMember }) {
     const ministereOptions = [
     "Intercession",
     "Louange",
+    "Administration",  
     "Technique",
     "Communication",
     "Les Enfants",

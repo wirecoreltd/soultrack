fdd2eeb93cfe90dc022f00d90bf4526ac8c57fbd
wirecoreltd <wirecoreltd@gmail.com>
2026-03-09 13:04:53 +0400

    Update list-members.js

--- pages/list-members.js
+++ pages/list-members.js
@@ -946,7 +946,7 @@ const canAddMember =
       
             {/* Nouveaux Membres */}
             {filteredNouveaux.length > 0 && (
-              <div className="px-2 py-1 rounded shadow text-white bg-[#2E3192] rounded">
+              <div className="px-2 py-1 rounded shadow text-white bg-transparent rounded">
                 💖 Bien aimé venu le {formatDate(filteredNouveaux[0].created_at)}
               </div>
             )}
@@ -954,7 +954,7 @@ const canAddMember =
             {filteredNouveaux.map((m) => (
               <div
                 key={m.id}
-                className="flex flex-col px-2 py-2 rounded-lg bg-white/15 hover:bg-blue-100/50 transition duration-150 border-l-4"
+                className="flex flex-row items-center px-2 py-2 rounded-lg bg-white/10 hover:bg-white/20 transition duration-150 gap-2 border-l-4" 
                 style={{ borderLeftColor: getBorderColor(m) }}>                             
 
                 {/* Ligne principale */}

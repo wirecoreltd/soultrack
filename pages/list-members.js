7a839a6ca3a604133e3b808a2a0725e1bf977a4a
wirecoreltd <wirecoreltd@gmail.com>
2026-03-09 15:42:17 +0400

    Update list-members.js

--- pages/list-members.js
+++ pages/list-members.js
@@ -577,7 +577,8 @@ const canAddMember =
    
         <div
   key={m.id}
-  className={`bg-white px-3 pb-3 pt-1 rounded-xl shadow-md border-l-4 relative ${getBorderClass(m)}`}
+  className="bg-white px-3 pb-3 pt-1 rounded-xl shadow-md border-l-4 relative"
+style={{ borderLeftColor: getBorderColor(m) }}
 >
           
           {/* Badge Nouveau */}

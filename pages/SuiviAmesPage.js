6695ef16d688cafbf72fe641b2b082fe4968c504
wirecoreltd <wirecoreltd@gmail.com>
2026-03-20 09:07:54 +0400

    Update SuiviAmesPage.js

--- pages/SuiviAmesPage.js
+++ pages/SuiviAmesPage.js
@@ -237,14 +237,11 @@ console.log("Conseiller query:", conseillerQuery, " | IDs dispo:", d.map(p => p.
       <div className="w-full max-w-7xl overflow-x-auto py-2">
         <div className="min-w-[1200px]">
           <div className="hidden sm:flex text-sm font-semibold uppercase text-white px-2 py-1 border-b border-gray-400 bg-transparent gap-y-2 text-center">
+        <div className="flex-[1]">Évangélisé</div>    
         <div className="flex-[2]">Nom complet</div>
-        <div className="flex-[1]">Envoi</div>
-        <div className="flex-[1]">Évangélisé<br/>Le</div>    
-        <div className="flex-[1]">Envoyé au<br/>Suivi</div>
-        
-            
+            <div className="flex-[1]">Envoi</div>
             <div className="flex-[1]">Jours</div>            
-            
+            <div className="flex-[1]">Envoyé au<br/>Suivi</div>
             <div className="flex-[1]">Statut (Envoyé)</div>
             <div className="flex-[1]">Date<br/>Intégration</div>
             <div className="flex-[1]">Baptisé<br/>le</div>
@@ -257,14 +254,12 @@ console.log("Conseiller query:", conseillerQuery, " | IDs dispo:", d.map(p => p.
           {filteredData.map((p) => (
             <div key={p.id} className="mb-1">
               <div className={`grid grid-cols-12 items-center px-2 py-2 rounded-lg bg-white/10 border-l-4 ${p.couleur}`}>
-            <div className="col-span-2 text-white text-center">{p.prenom} {p.nom}</div>
-            <div className="col-span-1 text-white text-center">{p.status_suivi}</div>
             <div className="col-span-1 text-white text-center">{new Date(p.created_at).toLocaleDateString()}</div>
-            <div className="col-span-1 text-white text-center">{p.lastSuivi?.status_suivis_evangelises || "-"}</div>               
-                
+                <div className="col-span-2 text-white text-center">{p.prenom} {p.nom}</div>
+                <div className="col-span-1 text-white text-center">{p.status_suivi}</div>
                 <div className="col-span-1 text-white text-center">{p.joursSansSuivi}</div>                
                 <div className="col-span-1 text-white text-center">{p.lastSuivi?.date_suivi ? new Date(p.lastSuivi.date_suivi).toLocaleDateString() : "-"}</div>
-                
+                <div className="col-span-1 text-white text-center">{p.lastSuivi?.status_suivis_evangelises || "-"}</div>
                 <div className="col-span-1 text-white text-center">{p.membre?.created_at ? new Date(p.membre.created_at).toLocaleDateString() : "-"}</div>
                 <div className="col-span-1 text-white text-center">{p.dateBapteme ? new Date(p.dateBapteme).toLocaleDateString() : "-"}</div>
                 <div className="col-span-1 text-white text-center">{p.debutMinistere ? new Date(p.debutMinistere).toLocaleDateString() : "-"}</div>

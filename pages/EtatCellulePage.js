69422f8059c64a4bd2113d2650e8a397095f06b8
wirecoreltd <wirecoreltd@gmail.com>
2026-03-26 13:33:10 +0400

    Update EtatCellulePage.js

--- pages/EtatCellulePage.js
+++ pages/EtatCellulePage.js
@@ -301,7 +301,7 @@ function EtatCellule() {
         </button>
       </div>
 
-  // ================== JSX KPI ==================
+  
 <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4 mt-6">
   {/* Total Évangélisés */}
   <div

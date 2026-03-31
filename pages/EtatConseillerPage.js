a3888195ba054cea7d372d9ad42d0301c693b87e
wirecoreltd <wirecoreltd@gmail.com>
2026-03-31 09:56:48 +0400

    Update EtatConseillerPage.js

--- pages/EtatConseillerPage.js
+++ pages/EtatConseillerPage.js
@@ -320,11 +320,11 @@ function EtatCellule() {
 
           {/* HEADER */}
           <div className="flex text-sm font-semibold uppercase text-white px-4 py-3 border-b border-white/30 bg-white/5 rounded-t-xl whitespace-nowrap">
-            <div className="min-w-[150px] ml-2">Date Depart</div>
+            <div className="min-w-[150px] ml-6">Date Depart</div>
             <div className="min-w-[200px] text-center">Nom Complet</div>
             <div className="min-w-[200px] text-center">Type</div>
             <div className="min-w-[200px] text-center">Statut</div>
-            <div className="min-w-[150px] text-center">Envoyer au Suivi Le</div>
+            <div className="min-w-[150px] text-center">Envoyer au </br>Suivi Le</div>
             <div className="min-w-[150px] text-center">Date Intégration</div>
             <div className="min-w-[150px] text-center">Date Baptême</div>
             <div className="min-w-[150px] text-center">Début Ministère</div>

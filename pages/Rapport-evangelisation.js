b584435387d50e91ad99e601efdd5499cbac2c58
wirecoreltd <wirecoreltd@gmail.com>
2026-04-03 22:24:11 +0400

    Update Rapport-evangelisation.js

--- pages/Rapport-evangelisation.js
+++ pages/Rapport-evangelisation.js
@@ -529,29 +529,29 @@ const handleConseillerClick = () => {
                   <div
                     className={`px-4 py-3 rounded-lg bg-white/25 cursor-pointer border-l-4 ${borderColor}`}
                     onClick={() => toggleMonth(monthKey)}
-                  >               
-                     
+                  >
+                    <div className="hidden md:flex items-center">
+                      <div className="min-w-[150px] text-white font-semibold">
+                        {isExpanded ? "➖ " : "➕ "} {monthLabel}
+                      </div>
+                      <div className="flex ml-auto text-white font-semibold text-sm">
+                        <div className="min-w-[110px] text-center ml-3">{monthTotals.hommes}</div>
+                        <div className="min-w-[110px] text-center">{monthTotals.femmes}</div>  
+                        <div className="min-w-[110px] text-center text-orange-400 font-semibold">
+                       {(monthTotals.hommes || 0) + (monthTotals.femmes || 0)} </div>
+                        <div className="min-w-[120px] text-center text-orange-400 font-semibold">{monthTotals.priere}</div>
+                        <div className="min-w-[140px] text-center">{monthTotals.nouveau}</div>
+                        <div className="min-w-[130px] text-center">{monthTotals.reconciliation}</div>
+                        <div className="min-w-[130px] text-center">{monthTotals.moissonneurs}</div>
+                        <div className="min-w-[120px]"></div>
+                      </div>
+                    </div>
+
                     {/* MOBILE */}
                     <div className="md:hidden text-white">
                         <div className="font-semibold">
                           {isExpanded ? "➖ " : "➕ "} {monthLabel}
                         </div>
-                      
-                        {/* ✅ AFFICHER LES STATS SEULEMENT SI OUVERT */}
-                        {isExpanded && (
-                          <div className="grid grid-cols-2 gap-1 text-sm mt-1">
-                            <div>Hommes: {monthTotals.hommes}</div>
-                            <div>Femmes: {monthTotals.femmes}</div>  
-                            <div className="font-semibold text-orange-400">
-                              Total: {(monthTotals.hommes || 0) + (monthTotals.femmes || 0)}
-                            </div>
-                            <div>Prières: {monthTotals.priere}</div>
-                            <div>NouvConv: {monthTotals.nouveau}</div>
-                            <div>Recon: {monthTotals.reconciliation}</div>
-                            <div>Moiss: {monthTotals.moissonneurs}</div>
-                          </div>
-                        )}   
-                      </div>
                     </div>
 
                   {/* TYPES */}

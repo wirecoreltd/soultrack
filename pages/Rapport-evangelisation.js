16308a9ad8c5a503545b4657b281e719c344e412
wirecoreltd <wirecoreltd@gmail.com>
2026-04-04 14:42:06 +0400

    Update Rapport-evangelisation.js

--- pages/Rapport-evangelisation.js
+++ pages/Rapport-evangelisation.js
@@ -631,7 +631,7 @@ const handleConseillerClick = () => {
                       typeReports.map((r) => (
                         <div
                           key={r.id}
-                          className={`px-4 py-2 rounded-lg bg-white/10 hover:bg-white/20 border-l-4 mt-2 ml-8 ${
+                          className={`px-4 py-2 rounded-lg bg-white/10 hover:bg-white/20 border-l-4 mt-1 ml-8 ${
                             typeColors[type] || "border-white"
                           }`}
                         >

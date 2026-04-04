ea7ac5b0aa2c44b01e2372458f64b0d2fa6fa4e0
wirecoreltd <wirecoreltd@gmail.com>
2026-04-04 13:37:31 +0400

    Update Rapport-evangelisation.js

--- pages/Rapport-evangelisation.js
+++ pages/Rapport-evangelisation.js
@@ -631,7 +631,9 @@ const handleConseillerClick = () => {
                       typeReports.map((r) => (
                         <div
                           key={r.id}
-                          className="px-4 py-2 rounded-lg bg-white/10 hover:bg-white/20 border-l-4 border-blue-500 ml-8"
+                          className={`px-4 py-2 rounded-lg bg-white/10 hover:bg-white/20 border-l-4 ml-8 ${
+                            typeColors[type] || "border-white"
+                          }`}
                         >
                           <div className="hidden md:flex items-center">
                             <div className="min-w-[150px] text-white">

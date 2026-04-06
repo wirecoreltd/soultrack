e28957295fa111a6e6de1d4de89d5157b8154be3
wirecoreltd <wirecoreltd@gmail.com>
2026-04-06 11:00:24 +0400

    Update cellules-hub.js

--- pages/cellules-hub.js
+++ pages/cellules-hub.js
@@ -121,6 +121,17 @@ function CellulesHubContent() {
                   Suivis des membres
                 </div>
               </Link>   
+
+                   <Link
+                href="/attendance_cellule"
+                className="flex-1 min-w-[250px] w-full h-32 bg-white rounded-3xl shadow-md flex flex-col justify-center items-center border-t-4 border-yellow-500 p-6 hover:shadow-xl transition-all duration-200 cursor-pointer"
+              >
+                <div className="text-5xl mb-2">👨‍👩‍👦‍👦</div>
+                <div className="text-lg font-bold text-gray-800 text-center">
+                  Présence Cellule
+                </div>
+              </Link> 
+                  
                <Link
                 href="/EtatCellulePage"
                 className="flex-1 min-w-[250px] w-full h-32 bg-white rounded-3xl shadow-md flex flex-col justify-center items-center border-t-4 border-yellow-500 p-6 hover:shadow-xl transition-all duration-200 cursor-pointer"

0f56d1c5a8d0a1b424b32a77d47789666d6cdddb
wirecoreltd <wirecoreltd@gmail.com>
2026-04-06 19:16:56 +0400

    Update Rapport-evangelisation.js

--- pages/Rapport-evangelisation.js
+++ pages/Rapport-evangelisation.js
@@ -537,7 +537,7 @@ const handleConseillerClick = () => {
 
             {/* HEADER MOIS */}
             <div
-              className={`px-4 py-3 rounded-lg bg-white/25 cursor-pointer border-l-4 ${borderColor}`}
+              className={`px-4 py-3 rounded-lg bg-white/20 cursor-pointer border-l-4 ${borderColor}`}
               onClick={() => toggleMonth(monthKey)}
             >
               <div className="hidden md:flex items-center">
@@ -580,7 +580,7 @@ const handleConseillerClick = () => {
                     {/* HEADER TYPE */}
                     <div
                       onClick={() => toggleType(typeKey)}
-                      className={`px-4 py-2 rounded-lg bg-white/15 cursor-pointer border-l-4 ml-4 ${
+                      className={`px-4 py-2 rounded-lg bg-white/20 cursor-pointer border-l-4 ml-4 ${
                         typeColors[type] || "border-white"
                       }`}
                     >
@@ -630,7 +630,7 @@ const handleConseillerClick = () => {
                       typeReports.map((r) => (
                         <div
                           key={r.id}
-                          className={`px-4 py-2 rounded-lg bg-white/10 hover:bg-white/20 border-l-4 mt-2 ml-8 ${
+                          className={`px-4 py-2 rounded-lg bg-white/20 hover:bg-white/20 border-l-4 mt-2 ml-8 ${
                             typeColors[type] || "border-white"
                           }`}
                         >

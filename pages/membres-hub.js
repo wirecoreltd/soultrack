2b0f0be7348598261324030209be5c786b906127
wirecoreltd <wirecoreltd@gmail.com>
2026-04-08 21:18:01 +0400

    Update membres-hub.js

--- pages/membres-hub.js
+++ pages/membres-hub.js
@@ -76,7 +76,7 @@ function MembresHubContent() {
 
         <Link href="/suivis-membres" className="flex-1 bg-white rounded-2xl shadow-md flex flex-col justify-center items-center border-t-4 border-[#38BDF8] p-6 hover:shadow-lg transition-all duration-200 cursor-pointer h-32">
           <div className="text-4xl mb-2">💌</div>
-          <div className="text-lg font-bold text-gray-800 text-center">Suivi de la Famille</div>
+          <div className="text-lg font-bold text-gray-800 text-center">Suivi des membres</div>
         </Link>
 
         <Link href="/create-conseiller" className="flex-1 bg-white rounded-2xl shadow-md flex flex-col justify-center items-center border-t-4 border-[#F97316] p-6 hover:shadow-lg transition-all duration-200 cursor-pointer h-32">

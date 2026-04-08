20945e5ec2ff515670595edd0237b4084c8d1b50
wirecoreltd <wirecoreltd@gmail.com>
2026-04-08 08:55:20 +0400

    Update EtatConseillerPage.js

--- pages/EtatConseillerPage.js
+++ pages/EtatConseillerPage.js
@@ -249,14 +249,14 @@ function EtatConseiller() {
       </h1>
 
       {/* FILTRES + GENERER + SELECT CONSEILLER */}
-        //<div className="bg-white/10 p-4 md:p-6 rounded-2xl shadow-lg mt-2 w-fit mx-auto flex flex-col md:flex-row items-center gap-3 md:gap-4 text-white">
+        
         <div className="bg-white/10 p-6 rounded-2xl shadow-lg mt-2 flex justify-center gap-4 flex-wrap text-white">
           
           <input 
             type="date" 
             value={filterDebut} 
             onChange={(e) => setFilterDebut(e.target.value)} 
-            //className="h-10 w-full md:w-auto border border-gray-400 rounded-lg px-3 py-2 bg-transparent text-white"
+           
           className="border border-gray-400 rounded-lg px-3 py-2 bg-transparent text-white"
               />
         
@@ -264,7 +264,7 @@ function EtatConseiller() {
             type="date" 
             value={filterFin} 
             onChange={(e) => setFilterFin(e.target.value)} 
-            //className="h-10 w-full md:w-auto border border-gray-400 rounded-lg px-3 py-2 bg-transparent text-white"
+           
          className="border border-gray-400 rounded-lg px-3 py-2 bg-transparent text-white"
               />  
         
@@ -280,7 +280,7 @@ function EtatConseiller() {
             <select
             value={filterConseiller}
             onChange={(e) => setFilterConseiller(e.target.value)}
-            //className="h-10 w-full md:w-auto border border-gray-400 rounded-lg px-3 py-2 bg-transparent text-white"
+          
          className="border border-gray-400 rounded-lg px-3 py-2 bg-transparent text-white"
               >
             <option value="" className="text-black">Tous les conseillers</option>

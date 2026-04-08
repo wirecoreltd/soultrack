bc86b7fc7affa8cf90672f73774c27ea916a43ee
wirecoreltd <wirecoreltd@gmail.com>
2026-04-08 11:55:28 +0400

    Update EtatConseillerPage.js

--- pages/EtatConseillerPage.js
+++ pages/EtatConseillerPage.js
@@ -249,27 +249,27 @@ function EtatConseiller() {
       </h1>
 
       {/* FILTRES + GENERER + SELECT CONSEILLER */}
-<div className="bg-white/10 p-4 md:p-6 rounded-2xl shadow-lg mt-2 w-fit mx-auto flex flex-col md:flex-row items-center gap-3 md:gap-4 text-white">
+<div className="bg-white/10 p-4 md:p-6 rounded-2xl shadow-lg mt-2 w-full md:w-fit md:mx-auto flex flex-col md:flex-row items-center gap-3 md:gap-4 text-white">
 
   <input 
     type="date" 
     value={filterDebut} 
     onChange={(e) => setFilterDebut(e.target.value)} 
-    className="h-10 w-full md:w-auto border border-gray-400 rounded-lg px-3 py-2 bg-transparent text-white"
+    className="order-1 h-10 w-full md:w-auto border border-gray-400 rounded-lg px-3 py-2 bg-transparent text-white"
   />
 
   <input 
     type="date" 
     value={filterFin} 
     onChange={(e) => setFilterFin(e.target.value)} 
-    className="h-10 w-full md:w-auto border border-gray-400 rounded-lg px-3 py-2 bg-transparent text-white"
+    className="order-1 h-10 w-full md:w-auto border border-gray-400 rounded-lg px-3 py-2 bg-transparent text-white"
   />  
 
   <button 
     onClick={fetchReports} 
-    className="h-10 w-fit bg-amber-300 text-white font-semibold px-6 rounded-lg hover:bg-amber-300 transition"
+    className="order-1 h-10 w-full md:w-auto border border-gray-400 rounded-lg px-3 py-2 bg-transparent text-white"
   >
-    Générer
+    {loading ? "Chargement..." : "Générer le rapport"}
   </button>
 
   {/* SELECT CONSEILLER (après génération) */}
@@ -277,7 +277,7 @@ function EtatConseiller() {
     <select
     value={filterConseiller}
     onChange={(e) => setFilterConseiller(e.target.value)}
-    className="h-10 w-full md:w-auto border border-gray-400 rounded-lg px-3 py-2 bg-transparent text-white"
+    className="order-4 w-full md:w-auto border border-gray-400 rounded-lg px-3 py-2 bg-transparent text-white"
   >
     <option value="" className="text-black">Tous les conseillers</option>
     {availableConseillers.map((c, i) => (<option key={i} value={c} className="text-black">{c}

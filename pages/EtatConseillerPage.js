8949c9f9381e9daaf50203699e5a1d0d4e7e8218
wirecoreltd <wirecoreltd@gmail.com>
2026-04-08 12:06:27 +0400

    Update EtatConseillerPage.js

--- pages/EtatConseillerPage.js
+++ pages/EtatConseillerPage.js
@@ -268,9 +268,9 @@ function EtatConseiller() {
 
   <button 
     onClick={fetchReports} 
-    className="order-1 h-10 w-full md:w-auto border border-gray-400 rounded-lg px-3 py-2 bg-amber-300 text-white hover:bg-amber-300 transition"
-  >
-    {loading ? "Chargement..." : "Générer le rapport"}
+    className="order-3 w-full md:w-auto h-10 bg-amber-300 text-white font-semibold px-6 rounded-lg hover:bg-amber-400 transition"
+          >
+            Générer
   </button>
 
   {/* SELECT CONSEILLER (après génération) */}

bd249ac63128c61286b6ac8c2cd0d983bcdd8753
wirecoreltd <wirecoreltd@gmail.com>
2026-04-08 11:29:42 +0400

    Update EtatConseillerPage.js

--- pages/EtatConseillerPage.js
+++ pages/EtatConseillerPage.js
@@ -253,26 +253,26 @@ function EtatConseiller() {
         <div className="bg-white/10 p-4 md:p-6 rounded-2xl shadow-lg mt-2 w-full md:w-fit md:mx-auto flex flex-col md:flex-row items-center gap-3 md:gap-4 text-white">
 
           {/* DATE DEBUT */}
-          <div className="flex flex-col">
-                      <label className="text-sm font-semibold mb-1">Date de début</label>
-          <input 
-            type="date" 
-            value={filterDebut} 
-            onChange={(e) => setFilterDebut(e.target.value)} 
-            className="order-1 w-full md:w-auto border border-gray-400 rounded-lg px-3 py-2 bg-transparent text-white"
-          />
-               </div>
+            <div className="flex flex-col items-center">
+              <label className="text-sm font-semibold mb-1 text-center">Date de début</label>
+              <input 
+                type="date" 
+                value={filterDebut} 
+                onChange={(e) => setFilterDebut(e.target.value)} 
+                className="order-1 w-full md:w-auto border border-gray-400 rounded-lg px-3 py-2 bg-transparent text-white"
+              />
+            </div>
         
           {/* DATE FIN */}
-  <div className="flex flex-col">
-                      <label className="text-sm font-semibold mb-1">Date de fin</label>
+   <div className="flex flex-col items-center">
+              <label className="text-sm font-semibold mb-1 text-center">Date de début</label>
           <input 
             type="date" 
             value={filterFin} 
             onChange={(e) => setFilterFin(e.target.value)} 
             className="order-2 w-full md:w-auto border border-gray-400 rounded-lg px-3 py-2 bg-transparent text-white"
           />
-                </div>
+              </div>
         
           {/* BOUTON GENERER */}
           <button 
@@ -284,8 +284,8 @@ function EtatConseiller() {
         
           {/* SELECT CONSEILLER */}
           {showTable && (
-            <div className="flex flex-col">
-                          <label className="text-sm font-semibold mb-1 text-white">Type de temps</label>
+             <div className="flex flex-col items-center">
+              <label className="text-sm font-semibold mb-1 text-center">Date de début</label>
             <select
               value={filterConseiller}
               onChange={(e) => setFilterConseiller(e.target.value)}
@@ -298,8 +298,9 @@ function EtatConseiller() {
                 </option>
               ))}
             </select>
-            </div>
-          )}        
+           </div>
+          )}
+        
         </div>
         
         {/* KPI (après génération) */}

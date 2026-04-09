6525fc41368d54e55c40babd40a06d6b0ac90862
wirecoreltd <wirecoreltd@gmail.com>
2026-04-09 10:40:09 +0400

    Update rapport-hub.js

--- pages/rapport-hub.js
+++ pages/rapport-hub.js
@@ -40,7 +40,7 @@ function RapportHubContent() {
 
       {/* 🔹 Liste des rapports */}
       <div className="text-center mb-6 max-w-5xl w-full">
-        <h2 className="text-xl font-bold mb-4 text-white">Liste des Rapports</h2>
+        <h2 className="text-xl font-bold mb-4 text-[#F43F5E]">Liste des Rapports</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
 
           <Link
@@ -122,25 +122,7 @@ function RapportHubContent() {
           >
             <div className="text-4xl mb-2">📊</div>
             <div className="text-lg font-bold text-gray-800 text-center">Statistiques Globales</div>
-          </Link>
-
-          <Link
-            href="/RapportConseilPage"
-            className="flex-1 bg-white rounded-2xl shadow-md flex flex-col justify-center items-center 
-            border-t-4 border-[#F97316] p-6 hover:shadow-lg transition-all duration-200 cursor-pointer h-32"
-          >
-            <div className="text-4xl mb-2">🧭</div>
-            <div className="text-lg font-bold text-gray-800 text-center">Rapport Conseillers</div>
-          </Link>
-
-          <Link
-            href="/RapportExtraPage"
-            className="flex-1 bg-white rounded-2xl shadow-md flex flex-col justify-center items-center 
-            border-t-4 border-[#14B8A6] p-6 hover:shadow-lg transition-all duration-200 cursor-pointer h-32"
-          >
-            <div className="text-4xl mb-2">✨</div>
-            <div className="text-lg font-bold text-gray-800 text-center">Rapport Extra</div>
-          </Link>
+          </Link>         
 
         </div>
       </div>

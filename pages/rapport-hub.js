3f814311ae9a79a42ee6eda9cd8c9bb42fae2b3d
wirecoreltd <wirecoreltd@gmail.com>
2026-04-09 10:28:12 +0400

    Update rapport-hub.js

--- pages/rapport-hub.js
+++ pages/rapport-hub.js
@@ -37,6 +37,9 @@ import ProtectedRoute from "../components/ProtectedRoute";
         </div>
       </div>
 
+<div className="text-center mb-6">
+        <h1 className="text-2xl font-bold mt-4 mb-6 text-blue-300 text-center text-white">Liste des Rapport</h1>
+
       {/* 🔹 Cartes */}
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4 w-full max-w-5xl mb-10">
 
@@ -47,63 +50,63 @@ import ProtectedRoute from "../components/ProtectedRoute";
           border-t-4 border-[#0D9488] p-6 hover:shadow-lg transition-all duration-200 cursor-pointer h-32"
         >
           <div className="text-4xl mb-2">⛪</div>
-          <div className="text-lg font-bold text-gray-800 text-center">Rapport du Culte</div>
+          <div className="text-lg font-bold text-gray-800 text-center">Présences & Statistiques</div>
         </Link>
 
         {/* Rapport évangélisation */}
         <Link
           href="/Rapport-evangelisation"
           className="flex-1 bg-white rounded-2xl shadow-md flex flex-col justify-center items-center 
-          border-t-4 border-[#FBBF24] p-6 hover:shadow-lg transition-all duration-200 cursor-pointer h-32"
+          border-t-4 border-[#3B82F6] p-6 hover:shadow-lg transition-all duration-200 cursor-pointer h-32"
         >
           <div className="text-4xl mb-2">🗣️</div>
-          <div className="text-lg font-bold text-gray-800 text-center">Rapport Évangélisation</div>
+          <div className="text-lg font-bold text-gray-800 text-center">Évangélisation</div>
         </Link>
 
         {/* Rapport baptême */}
         <Link
           href="/RapportBaptemePage"
           className="flex-1 bg-white rounded-2xl shadow-md flex flex-col justify-center items-center 
-          border-t-4 border-[#3B82F6] p-6 hover:shadow-lg transition-all duration-200 cursor-pointer h-32"
+          border-t-4 border-[#F59E0B] p-6 hover:shadow-lg transition-all duration-200 cursor-pointer h-32"
         >
           <div className="text-4xl mb-2">💧</div>
-          <div className="text-lg font-bold text-gray-800 text-center">Rapport Baptêmes</div>
+          <div className="text-lg font-bold text-gray-800 text-center">Baptêmes</div>
         </Link>
 
         {/* Rapport Formation */}
         <Link
           href="/RapportFormationPage"
           className="flex-1 bg-white rounded-2xl shadow-md flex flex-col justify-center items-center 
-          border-t-4 border-[#3B82F6] p-6 hover:shadow-lg transition-all duration-200 cursor-pointer h-32"
+          border-t-4 border-[#10B981] p-6 hover:shadow-lg transition-all duration-200 cursor-pointer h-32"
         >
           <div className="text-4xl mb-2">✒️</div>
-          <div className="text-lg font-bold text-gray-800 text-center">Rapport Formation</div>
+          <div className="text-lg font-bold text-gray-800 text-center">Formation</div>
         </Link>
 
         {/* Rapport Ministere */}
         <Link
           href="/RapportMinisterePage"
           className="flex-1 bg-white rounded-2xl shadow-md flex flex-col justify-center items-center 
-          border-t-4 border-[#3B82F6] p-6 hover:shadow-lg transition-all duration-200 cursor-pointer h-32"
+          border-t-4 border-[#EF4444] p-6 hover:shadow-lg transition-all duration-200 cursor-pointer h-32"
         >
           <div className="text-4xl mb-2">💢</div>
-          <div className="text-lg font-bold text-gray-800 text-center">Rapport par Ministère</div>
+          <div className="text-lg font-bold text-gray-800 text-center">Ministère</div>
         </Link>
 
         {/* Rapport Besoin */}
         <Link
           href="/RapportBesoinPage"
           className="flex-1 bg-white rounded-2xl shadow-md flex flex-col justify-center items-center 
-          border-t-4 border-[#3B82F6] p-6 hover:shadow-lg transition-all duration-200 cursor-pointer h-32"
+          border-t-4 border-[#8B5CF6] p-6 hover:shadow-lg transition-all duration-200 cursor-pointer h-32"
         >
           <div className="text-4xl mb-2">❓</div>
-          <div className="text-lg font-bold text-gray-800 text-center">Rapport par Difficultés / Besoins</div>
+          <div className="text-lg font-bold text-gray-800 text-center">Difficultés / Besoins</div>
         </Link>
             
         <Link
           href="/EtatConseillerPage"
           className="flex-1 bg-white rounded-2xl shadow-md flex flex-col justify-center items-center 
-          border-t-4 border-[#3B82F6] p-6 hover:shadow-lg transition-all duration-200 cursor-pointer h-32"
+          border-t-4 border-[#F43F5E] p-6 hover:shadow-lg transition-all duration-200 cursor-pointer h-32"
         >
           <div className="text-4xl mb-2">🌱</div>
           <div className="text-lg font-bold text-gray-800 text-center">L'évolution des Ames par Conseiller</div>
@@ -112,7 +115,7 @@ import ProtectedRoute from "../components/ProtectedRoute";
          <Link
           href="/EtatCellulePage"
           className="flex-1 bg-white rounded-2xl shadow-md flex flex-col justify-center items-center 
-          border-t-4 border-[#3B82F6] p-6 hover:shadow-lg transition-all duration-200 cursor-pointer h-32"
+          border-t-4 border-[#EAB308] p-6 hover:shadow-lg transition-all duration-200 cursor-pointer h-32"
         >
           <div className="text-4xl mb-2">🪴</div>
           <div className="text-lg font-bold text-gray-800 text-center">Etat Cellule</div>
@@ -123,19 +126,20 @@ import ProtectedRoute from "../components/ProtectedRoute";
         <Link
           href="/StatGlobalPage"
           className="flex-1 bg-white rounded-2xl shadow-md flex flex-col justify-center items-center 
-          border-t-4 border-[#10B981] p-6 hover:shadow-lg transition-all duration-200 cursor-pointer h-32"
+          border-t-4 border-[#6366F1] p-6 hover:shadow-lg transition-all duration-200 cursor-pointer h-32"
         >
           <div className="text-4xl mb-2">📊</div>
           <div className="text-lg font-bold text-gray-800 text-center">Statistiques Globales</div>
         </Link>
+      </div>     
 
-      </div>
-
-      {/* 🔹 Verset biblique */}
-      <div className="mt-auto mb-4 text-center text-white text-lg italic max-w-2xl leading-relaxed tracking-wide font-light">
-        Et le Seigneur ajoutait chaque jour à l’Église ceux qui étaient sauvés. <br />
-        Actes 2:47 ✨
-      </div>
+        {/* Texte motivant en bas */}
+        <div className="max-w-3xl w-full mb-6 text-center">
+          <p className="italic text-base text-white/90">
+            Et le Seigneur ajoutait chaque jour à l’Église ceux qui étaient sauvés.  <br />
+            Actes 2:47
+          </p>
+        </div>
     </div>
   );
 }

5a96e78d591deb12d5cb7be2bd732b579b425b47
wirecoreltd <wirecoreltd@gmail.com>
2026-04-09 13:46:23 +0400

    Update administrateur.js

--- pages/administrateur.js
+++ pages/administrateur.js
@@ -58,9 +58,21 @@ export default function Administrateur() {
         <HeaderPages />
 
         {/* 🔹 Titre */}
-        <h1 className="text-3xl font-bold text-amber-300 mb-8">
-          Espace Admin
-        </h1>
+         <h1 className="text-2xl font-bold mt-4 mb-6 text-blue-300 text-center text-white">Espace Administrateur</h1>  
+
+  <div className="max-w-3xl w-full mb-6 text-center">
+          <p className="italic text-base text-white/90">
+        Gérez l’ensemble de votre plateforme en toute simplicité.  
+    Depuis cet espace, vous pouvez organiser les{" "}
+    <span className="text-amber-300 font-semibold">utilisateurs</span>, structurer les{" "}
+    <span className="text-amber-300 font-semibold">cellules</span> et gérer les{" "}
+    <span className="text-amber-300 font-semibold">liens entre églises</span>.  
+  </p>
+
+  <p className="italic text-base text-white/90 mt-2">
+    Chaque action contribue à une meilleure organisation et à une croissance harmonieuse de l’ensemble. ⚙️
+  </p>
+        </div>
 
         {/* 🔹 Cartes principales */}
         <div className="flex flex-col md:flex-row gap-6 justify-center w-full max-w-5xl mb-6 flex-wrap">

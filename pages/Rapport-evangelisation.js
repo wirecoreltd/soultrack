0414347a16603237d339a5d1896637a990d11480
wirecoreltd <wirecoreltd@gmail.com>
2026-04-09 22:41:12 +0400

    Update Rapport-evangelisation.js

--- pages/Rapport-evangelisation.js
+++ pages/Rapport-evangelisation.js
@@ -343,7 +343,7 @@ const handleConseillerClick = () => {
   className="bg-white/10 p-4 md:p-6 rounded-2xl shadow-lg mt-2 w-full md:w-fit md:mx-auto flex flex-col text-white">
   
   {/* TEXTE AU-DESSUS */}
-  <p className="text-red-500 font-semibold text-center mb-4">
+  <p className="text-red-300 font-semibold text-center mb-4">
     Choisissez les paramètres pour générer le rapport
   </p>
 
@@ -352,7 +352,7 @@ const handleConseillerClick = () => {
 
     {/* Date début */}
     <div className="flex flex-col w-full md:w-auto">
-      <label className="text-xs text-center mb-1">Date début</label>
+      <label className="text-base text-center mb-1">Date début</label>
       <input
         type="date"
         value={dateDebut}

9d12d0976e3739bee0cc1521be515f365b68d145
wirecoreltd <wirecoreltd@gmail.com>
2026-04-09 12:02:50 +0400

    Update link-eglise.js

--- pages/admin/link-eglise.js
+++ pages/admin/link-eglise.js
@@ -220,8 +220,7 @@ Que Dieu vous bénisse 🙏
     <div className="min-h-screen bg-[#333699] text-white p-4 flex flex-col items-center">
       <HeaderPages />     
 
-         {/* TEXTE EXPLICATIF FULL WIDTH, CENTRÉ, COULEURS POUR ACTIONS */}
-    {/* TEXTE EXPLICATIF FULL WIDTH, CENTRÉ, COULEURS POUR ACTIONS */}
+         {/* TEXTE EXPLICATIF FULL WIDTH, CENTRÉ, COULEURS POUR ACTIONS */}    
 <div className="w-full flex flex-col items-center mb-8">
   <h1 className="text-2xl font-bold mt-4 mb-6 text-center text-white">
     Invitations & Liens <span className="text-emerald-300">d’Eglises</span>
@@ -232,7 +231,7 @@ Que Dieu vous bénisse 🙏
       Relier une église vous permet de placer une église sous votre supervision et de voir ses statistiques dans <span className="text-blue-300 font-semibold">Stats Globales</span>.
       </p>
        
-    <p className="text-white/80 mb-2">
+    <p className="text-white 3xl mb-2 text-semibold">
       Dans cette interface, vous pouvez :
     </p>
     
@@ -240,7 +239,7 @@ Que Dieu vous bénisse 🙏
       <li className="text-green-400">✉️ <strong>Envoyer une invitation</strong> à une église</li>  
       <li className="text-gray-400">🔗 <strong>Casser le lien</strong> avec une église supervisée</li>
       <li className="text-red-500">🗑️ <strong>Supprimer</strong> une invitation envoyée</li>
-      <li className="text-green-400">🔄 <strong>Renvoyer le lien</strong> si nécessaire</li>
+      <li className="text-orange-400">🔄 <strong>Renvoyer le lien</strong> si nécessaire</li>
       <li className="text-yellow-300">⏳ <strong>Envoyer un rappel</strong> pour une invitation en attente</li>
     </ul>
     

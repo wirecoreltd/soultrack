d3f1c50e6b5733397cbd4b6a75f0891ae2b4e638
wirecoreltd <wirecoreltd@gmail.com>
2026-04-13 20:25:34 +0400

    Update RapportBaptemePage.js

--- pages/RapportBaptemePage.js
+++ pages/RapportBaptemePage.js
@@ -517,8 +517,8 @@ Enregistrez les données, <span className="text-blue-300 font-semibold">analysez
                       <span className="text-[10px]">{isOpen ? "➖" : "➕"}</span>
                       {group.label}
                     </span>
-                    <span className="text-orange-300 font-semibold">
-                      {monthTotal.hommes}H / {monthTotal.femmes}F = {monthTotal.hommes + monthTotal.femmes}
+                    <span className="text-orange-400 font-semibold">
+                      H: {monthTotal.hommes} | F: {monthTotal.femmes} = {monthTotal.hommes + monthTotal.femmes}
                     </span>
                   </button>
 
@@ -559,7 +559,7 @@ Enregistrez les données, <span className="text-blue-300 font-semibold">analysez
             {/* TOTAL GLOBAL MOBILE */}
             <div className="flex items-center px-4 py-3 mt-2 border-t border-white/50 bg-white/10 rounded-b-xl justify-center">
               <span className="text-white font-bold mr-4">TOTAL</span>
-              <span className="text-orange-400 font-semibold">Total H + {totalGlobal.hommes} / F :{totalGlobal.femmes} = {totalGlobal.hommes+totalGlobal.femmes}</span>
+              <span className="text-orange-400 font-semibold">Total (H:{totalGlobal.hommes} + F:{totalGlobal.femmes}) = {totalGlobal.hommes+totalGlobal.femmes}</span>
             </div>
 
           </div>

cb5833cf542e7008d4845a2f27f158ab9c8f72de
wirecoreltd <wirecoreltd@gmail.com>
2026-04-14 23:35:53 +0400

    Update RapportBaptemePage.js

--- pages/RapportBaptemePage.js
+++ pages/RapportBaptemePage.js
@@ -569,24 +569,47 @@ function RapportBaptemes() {
               </div>
             </button>
 
-            {isOpen && group.items.map(r => {
-              const total = Number(r.hommes) + Number(r.femmes);
-              return (
-                <div key={r.id} className="bg-white/10 p-2 rounded">
-                  {r.baptise_par} - {total}
+             {/* DETAILS — même couleur que le mois */}
+                  {isOpen && group.items.map((r) => {
+                    const total = Number(r.hommes) + Number(r.femmes);
+                    return (
+                      <div
+                        key={r.id + r.baptise_par}
+                        className="bg-white/10 text-white rounded-xl px-4 py-2 flex flex-col gap-2 shadow mb-2"
+                      >
+                        <div className="text-amber-300 text-right">
+                          {formatDateFR(r.date)}
+                        </div>
+                        <div className="text-white">
+                          Baptisé par : <span className="font-semibold">{r.baptise_par}</span>
+                        </div>
+                        <div className="text-white">
+                          Hommes : {r.hommes} | Femmes : {r.femmes}
+                        </div>
+                        <div className="text font-semibold text-orange-400">
+                          Total : {total}
+                        </div>
+                        <button
+                          onClick={() => handleEdit(r)}
+                          className="mx-auto text-amber-300 mt-2 hover:scale-110 transition"
+                        >
+                          ✏️ Modifier
+                        </button>
+                      </div>
+                    );
+                  })}
+
                 </div>
               );
             })}
+
+            {/* TOTAL GLOBAL MOBILE — une seule fois à la fin */}
+            <div className="flex items-center px-4 py-3 rounded-lg bg-white/10 hover:bg-white/20 transition border-l-4 cursor-pointer mb-2 border-orange-400">
+              <span className="text-orange-400 font-semibold mr-4">TOTAL</span>
+              <span className="text-orange-400 font-semibold">(H:{totalGlobal.hommes} + F:{totalGlobal.femmes}) = {totalGlobal.hommes + totalGlobal.femmes}</span>
+            </div>
+
           </div>
-        );
-      })}
-
-      {/* TOTAL */}
-      <div className="text-orange-400 mt-2">
-        TOTAL: {totalGlobal.hommes + totalGlobal.femmes}
-      </div>
-
-    </div>
   </>
 )}
       

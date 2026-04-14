062abf1164aa03f5eca48e44dfc75bbe4ba37058
wirecoreltd <wirecoreltd@gmail.com>
2026-04-14 10:35:00 +0400

    Update RapportBaptemePage.js

--- pages/RapportBaptemePage.js
+++ pages/RapportBaptemePage.js
@@ -36,8 +36,7 @@ function RapportBaptemes() {
   const router = useRouter();
   const formRef=useRef(null);
   const [rapportSuccess, setRapportSuccess] = useState(false);
-  const borderColor = getMonthColor(group.key);
-
+  
   /* USER */
   useEffect(()=>{
     const fetchUser=async()=>{
@@ -455,10 +454,12 @@ Enregistrez les données, <span className="text-blue-300 font-semibold">analysez
     <div key={group.key}>
 
       {/* MOIS */}
-      <button
-        onClick={() => toggleMonth(group.key)}
-        className={`flex items-center px-4 py-3 rounded-lg bg-white/10 hover:bg-white/20 transition border-l-4 cursor-pointer mb-2 ${borderColor}`}
-      >
+      const borderColor = getMonthColor(group.key);
+
+          <button
+            onClick={() => toggleMonth(group.key)}
+            className={`flex items-center px-4 py-3 rounded-lg bg-white/10 hover:bg-white/20 transition border-l-4 cursor-pointer mb-2 ${borderColor}`}
+          >
         <div className="min-w-[260px] text-white font-semibold flex items-center gap-2">
           <span>{isOpen ? "➖" : "➕"}</span>
           <span>{group.label}</span>
@@ -486,38 +487,23 @@ Enregistrez les données, <span className="text-blue-300 font-semibold">analysez
         const total = Number(r.hommes) + Number(r.femmes);
 
         return (
-          <div key={r.id} className="pl-6">
-            <div className={`flex items-stretch px-4 py-2 rounded-xl bg-white/10 hover:bg-white/20 transition border-l-4 mb-1 border border-white/20 ${borderColor}`}>
+         const borderColor = getMonthColor(group.key);
 
-              <div className="min-w-[180px] text-white">
-                {formatDateFR(r.date)}
-              </div>
-
-              <div className="min-w-[180px] text-center text-white">
-                {r.baptise_par}
-              </div>
-
-              <div className="min-w-[120px] text-center text-white">
-                {r.hommes}
-              </div>
-
-              <div className="min-w-[120px] text-center text-white">
-                {r.femmes}
-              </div>
-
-              <div className="min-w-[120px] text-center text-white font-bold">
-                {total}
-              </div>
-
-              <div className="min-w-[150px] text-center">
-                <button
-                  onClick={() => handleEdit(r)}
-                  className="text-orange-400 underline"
+              <div className="pl-6">
+                <div
+                  className={`flex items-stretch px-4 py-2 rounded-xl bg-white/10 hover:bg-white/20 transition border-l-4 mb-1 border border-white/20 ${borderColor}`}
                 >
+
+              <div className="min-w-[180px] text-white">{formatDateFR(r.date)} </div>
+              <div className="min-w-[180px] text-center text-white">{r.baptise_par}</div>
+              <div className="min-w-[120px] text-center text-white">{r.hommes}</div>
+              <div className="min-w-[120px] text-center text-white">{r.femmes}</div>
+              <div className="min-w-[120px] text-center text-white font-bold">{total}</div>
+              <div className="min-w-[150px] text-center">
+              <button onClick={() => handleEdit(r)} className="text-orange-400 underline">
                   Modifier
                 </button>
               </div>
-
             </div>
           </div>
         );

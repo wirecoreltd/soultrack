5571acf134a41d8753b0944ff774c7fe40022907
wirecoreltd <wirecoreltd@gmail.com>
2026-04-17 09:16:16 +0400

    Update HomePage.js

--- pages/site/HomePage.js
+++ pages/site/HomePage.js
@@ -2,8 +2,8 @@
 
 import { useRouter } from "next/navigation";
 import Image from "next/image";
-import PublicHeader from "../components/PublicHeader";
-import Footer from "../components/Footer";
+import PublicHeader from "../../components/PublicHeader";
+import Footer from "../../components/Footer";
 
 export default function HomePage() {
   const router = useRouter();

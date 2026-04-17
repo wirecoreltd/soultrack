23d72c600f009038d101d38177273fef2d812e49
wirecoreltd <wirecoreltd@gmail.com>
2026-04-17 11:36:49 +0400

    Update layout.tsx

--- layout.tsx
+++ layout.tsx
@@ -1,22 +1,19 @@
-import {NextIntlClientProvider} from 'next-intl';
-import {notFound} from 'next/navigation';
+import LanguageSwitcher from '@/components/LanguageSwitcher';
 
 export default async function LocaleLayout({children, params}) {
   const {locale} = params;
 
-  let messages;
-  try {
-    messages = (await import(`../../messages/${locale}.json`)).default;
-  } catch {
-    notFound();
-  }
+  let messages = (await import(`../../messages/${locale}.json`)).default;
 
   return (
     <html lang={locale}>
       <body>
-        <NextIntlClientProvider messages={messages}>
-          {children}
-        </NextIntlClientProvider>
+        <PublicHeader />
+        <header className="relative">
+          <LanguageSwitcher />
+        </header>
+
+        {children}
       </body>
     </html>
   );

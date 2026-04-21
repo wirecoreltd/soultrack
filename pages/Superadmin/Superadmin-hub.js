6ef8007b25d4253586c67b54c71fc4c80faef5fd
wirecoreltd <wirecoreltd@gmail.com>
2026-04-21 12:50:55 +0400

    Update Superadmin-hub.js

--- pages/Superadmin/Superadmin-hub.js
+++ pages/Superadmin/Superadmin-hub.js
@@ -57,39 +57,17 @@ function SuperadminHubContent() {
       <HeaderPages />
 
       <div className="text-center mb-6">
-         <h1 className="text-2xl font-bold mt-4 mb-6 text-blue-300 text-center text-white">Espace Membres</h1>
-      
-        <div className="max-w-3xl w-full mb-6 text-center">
-          <p className="italic text-base text-white/90">
-          Accédez facilement à toutes les <span className="text-blue-300 font-semibold"> fonctionnalités</span> de votre 
-         équipe : gestion des membres, suivi des familles, création et suivi des conseillers, ainsi que les rapports et présences. <span className="text-blue-300 font-semibold"> Tout votre suivi au même endroit</span>.      
-          </p>
-        </div>
-      </div>   
+         <h1 className="text-2xl font-bold mt-4 mb-6 text-blue-300 text-center text-white">Espace Super Admin</h1>     
+    </div>   
 
       {/* Cartes principales */}
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4 w-full max-w-5xl mb-6">
         <Link href="/Superadmin/temoignages" className="flex-1 bg-white rounded-2xl shadow-md flex flex-col justify-center items-center border-t-4 border-[#0D9488] p-6 hover:shadow-lg transition-all duration-200 cursor-pointer h-32">
           <div className="text-4xl mb-2">💡</div>
           <div className="text-lg font-bold text-gray-800 text-center">Témoignages</div>
-        </Link>
-        
+        </Link>        
       </div>
-
-      {/* Bouton popup pour envoyer le lien */}
-      <div className="w-full max-w-md mb-10">
-        <SendLinkPopup
-          label="Envoyer l'appli – Nouveau membre"
-          type="ajouter_membre"
-          buttonColor="from-[#09203F] to-[#537895]"
-          userId={userId} // 🔹 passer le userId
-        />
-      </div>
-
-      <div className="mt-auto mb-4 text-center text-white text-base italic max-w-2xl leading-relaxed tracking-wide font-light">
-        Car le corps ne se compose pas d’un seul membre, mais de plusieurs. <br />
-        1 Corinthiens 12:14 ❤️
-      </div>
+      
       <Footer />
     </div>
   );

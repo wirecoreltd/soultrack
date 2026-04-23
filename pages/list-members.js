7a16f9b2d49028e228cc10b03c40b9e4887b218f
wirecoreltd <wirecoreltd@gmail.com>
2026-04-23 11:07:11 +0400

    Update list-members.js

--- pages/list-members.js
+++ pages/list-members.js
@@ -888,14 +888,16 @@ const canAddMember =
 
       <p>❤️‍🩹 Suivi pastoral : {m.Soin_Pastoral || "—"}</p>
       <p>❓ Besoins : {besoins}</p>
-        <button onClick={...}>
-      💡 Ajouter / Voir suivis
-    </button>
-
+       <button
+  onClick={() => setOpenSuivi(true)}
+  className="mt-2 text-sm bg-[#25297e] text-white px-3 py-1 rounded"
+>
+  💡 Ajouter / Voir suivis
+</button>
     {/* popup */}
     {openSuivi && (
       <SuiviPopup member={m} onClose={...} />
-    )}  
+    )}
     </div>    
              <div className="flex flex-col items-center">                    
                  <div className="flex flex-col items-center w-full p-4 bg-white rounded-lg shadow-md space-y-2">

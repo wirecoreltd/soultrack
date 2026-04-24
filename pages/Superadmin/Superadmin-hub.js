121aefecf2c4ee20eb5fd87086bfb272c0b1dc0f
wirecoreltd <wirecoreltd@gmail.com>
2026-04-24 16:44:32 +0400

    Update Superadmin-hub.js

--- pages/Superadmin/Superadmin-hub.js
+++ pages/Superadmin/Superadmin-hub.js
@@ -65,7 +65,11 @@ function SuperadminHubContent() {
         <Link href="/Superadmin/temoignages" className="flex-1 bg-white rounded-2xl shadow-md flex flex-col justify-center items-center border-t-4 border-[#0D9488] p-6 hover:shadow-lg transition-all duration-200 cursor-pointer h-32">
           <div className="text-4xl mb-2">💡</div>
           <div className="text-lg font-bold text-gray-800 text-center">Témoignages</div>
-        </Link>        
+        </Link>   
+        <Link href="/Superadmin/AsminDashboard" className="flex-1 bg-white rounded-2xl shadow-md flex flex-col justify-center items-center border-t-4 border-[#0D9488] p-6 hover:shadow-lg transition-all duration-200 cursor-pointer h-32">
+          <div className="text-4xl mb-2">🏆</div>
+          <div className="text-lg font-bold text-gray-800 text-center">Eglise Count</div>
+        </Link> 
       </div>
       
       <Footer />

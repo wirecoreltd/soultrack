b7f0491efe1aaf7557126e389cb14cef53681dcb
wirecoreltd <wirecoreltd@gmail.com>
2026-04-28 16:28:31 +0400

    Update familles-hub.js

--- pages/familles-hub.js
+++ pages/familles-hub.js
@@ -69,7 +69,7 @@ useEffect(() => {
 }, []);
 
   return (
-    <AccessGuard allowedRoles={["Administrateur", "ResponsableFamilles"]}>
+   <ProtectedRoute allowedRoles={["Administrateur", "ResponsableFamilles", "Superadmin", "ResponsableCellule", "Membre"]}>
       <div
         className="min-h-screen flex flex-col items-center p-6"
         style={{ background: "linear-gradient(135deg, #2E3192 0%, #92EFFD 100%)" }}

cb4c193fb79bc578cd9eb6b60837fb5055f3091c
wirecoreltd <wirecoreltd@gmail.com>
2026-04-28 17:14:21 +0400

    Update familles-hub.js

--- pages/familles-hub.js
+++ pages/familles-hub.js
@@ -91,7 +91,7 @@ useEffect(() => {
         <div className="flex flex-col md:flex-row gap-6 justify-center w-full max-w-5xl mb-6 flex-wrap">
         
           <Link
-            href="/admin/list-Familles"
+            href="/admin/list-familles"
             className="flex-1 min-w-[250px] w-full h-32 bg-white rounded-3xl shadow-md flex flex-col justify-center items-center border-t-4 border-blue-500 p-6 hover:shadow-xl transition-all duration-200 cursor-pointer"
           >
             <div className="text-5xl mb-2">🏠</div>

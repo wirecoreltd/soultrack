1aacf8e373012e6dd05f7eea411de8165d7b46d5
wirecoreltd <wirecoreltd@gmail.com>
2026-04-29 17:40:21 +0400

    Update and rename pages/attendance_cellule.js to pages/cellule/attendance_cellule.js

--- pages/cellule/attendance_cellule.js
+++ pages/cellule/attendance_cellule.js
@@ -1,10 +1,10 @@
 "use client";
 
 import { useState, useEffect, useRef } from "react";
-import supabase from "../lib/supabaseClient";
-import HeaderPages from "../components/HeaderPages";
-import Footer from "../components/Footer";
-import ProtectedRoute from "../components/ProtectedRoute";
+import supabase from "../../lib/supabaseClient";
+import HeaderPages from "../../components/HeaderPages";
+import Footer from "../../components/Footer";
+import ProtectedRoute from "../../components/ProtectedRoute";
 
 export default function AttendancePage() {
   return (

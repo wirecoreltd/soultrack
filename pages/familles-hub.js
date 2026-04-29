76da05ee95db483b4126eaceabbe5daa0cbe5d31
wirecoreltd <wirecoreltd@gmail.com>
2026-04-29 13:14:43 +0400

    Update familles-hub.js

--- pages/familles-hub.js
+++ pages/familles-hub.js
@@ -141,7 +141,7 @@ useEffect(() => {
           </Link>   
 
           <Link
-            href="/attendance_cellule"
+            href="/attendance_famille"
             className="flex-1 min-w-[250px] w-full h-32 bg-white rounded-3xl shadow-md flex flex-col justify-center items-center border-t-4 border-yellow-500 p-6 hover:shadow-xl transition-all duration-200 cursor-pointer"
           >
             <div className="text-5xl mb-2">👨‍👩‍👦‍👦</div>

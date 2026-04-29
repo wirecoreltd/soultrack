214f41f694f35a2176f473ecfd1c3be911f0bd5c
wirecoreltd <wirecoreltd@gmail.com>
2026-04-29 09:30:40 +0400

    Update link and text for family members section

--- pages/familles-hub.js
+++ pages/familles-hub.js
@@ -111,12 +111,12 @@ useEffect(() => {
           </Link>
         
           <Link
-            href="/membres-cellule"
+            href="/membres-famille"
             className="flex-1 min-w-[250px] w-full h-32 bg-white rounded-3xl shadow-md flex flex-col justify-center items-center border-t-4 border-green-500 p-6 hover:shadow-xl transition-all duration-200 cursor-pointer"
           >
             <div className="text-5xl mb-2">👥</div>
             <div className="text-lg font-bold text-gray-800 text-center">
-              Membres de la Cellule
+              Membres de ma Famille
             </div>
           </Link>
         

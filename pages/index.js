d3d954b5d1c8017355ce135a260bebeb5b922175
wirecoreltd <wirecoreltd@gmail.com>
2026-05-04 08:47:19 +0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ const roleCards = {
     { path: "/conseiller/conseiller-hub", label: "Conseiller", emoji: "🤝", color: "#0EA5E9" },
     { path: "/famille/familles-hub", label: "Familles", emoji: "👑", color: "#F59E0B" },
     { path: "/rapport/rapport-hub", label: "Rapport", emoji: "📈", color: "#FBBF24" },
-    { path: "/admin/administrateur", label: "Admin", emoji: "⚙️", color: "#0EA5E9" },     
+    { path: "/administrateur/administrateur", label: "Admin", emoji: "⚙️", color: "#0EA5E9" },     
     { path: "/Presence", label: "Presence", emoji: "✍🏻", color: "#0EA5E9" },     
       ],
   
@@ -25,7 +25,7 @@ const roleCards = {
     { path: "/conseiller/conseiller-hub", label: "Conseiller", emoji: "🤝", color: "#0EA5E9" },
     { path: "/famille/familles-hub", label: "Familles", emoji: "👑", color: "#F59E0B" }, 
     { path: "/rapport/rapport-hub", label: "Rapport", emoji: "📈", color: "#FBBF24" },
-    { path: "admin/administrateur", label: "Admin", emoji: "⚙️", color: "#0EA5E9" },    
+    { path: "/administrateur/administrateur", label: "Admin", emoji: "⚙️", color: "#0EA5E9" },    
      { path: "/Presence", label: "Presence", emoji: "✍🏻", color: "#0EA5E9" },
     { path: "/Superadmin/Superadmin-hub", label: "Admin SoulTrack", emoji: "🔐", color: "#000000" },
   ],

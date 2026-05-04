3b25324b4ff8fe91b200744a7093df0a975f3941
wirecoreltd <wirecoreltd@gmail.com>
2026-05-04 09:58:33 +0400

    Update list-members.js

--- pages/membres/list-members.js
+++ pages/membres/list-members.js
@@ -912,6 +912,11 @@ function ListMembersContent() {
     </>
   )}
 </div>
+{showImport && userProfile && (
+  <div className="w-full max-w-3xl mx-auto mt-4">
+    <ImportMembresCSV user={userProfile} />
+  </div>
+)}
 
       {view === "card" && (
         <>

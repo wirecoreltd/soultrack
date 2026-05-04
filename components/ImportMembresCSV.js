72d8d8bdb632b9c28304029cea29c9a745542a41
wirecoreltd <wirecoreltd@gmail.com>
2026-05-04 13:41:26 +0400

    Update ImportMembresCSV.js

--- components/ImportMembresCSV.js
+++ components/ImportMembresCSV.js
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import supabase from "../../lib/supabaseClient";
+import supabase from "../lib/supabaseClient";
 import Papa from "papaparse";
 
 export default function ImportMembresCSV({ user }) {

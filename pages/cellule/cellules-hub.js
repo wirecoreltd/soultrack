dd2286f60720ae8bafed41902bed200e6732a4f4
wirecoreltd <wirecoreltd@gmail.com>
2026-05-05 16:19:08 +0400

    Update cellules-hub.js

--- pages/cellule/cellules-hub.js
+++ pages/cellule/cellules-hub.js
@@ -174,13 +174,8 @@ function CellulesHubContent() {
           <div className="text-lg font-bold text-gray-800 text-center">Etat Cellule</div>
         </Link>
 
-          <Link href="/Presence" className="flex-1 min-w-[250px] w-full h-32 bg-white rounded-3xl shadow-md flex flex-col justify-center items-center border-t-4 border-yellow-500 p-6 hover:shadow-xl transition-all duration-200 cursor-pointer">
-                    <div className="text-5xl mb-2">✍️</div>
-                    <div className="text-lg font-bold text-gray-800 text-center">Présence</div>
-                  </Link>
-
           <Link href="/admin/notifications" className="flex-1 min-w-[250px] w-full h-32 bg-white rounded-3xl shadow-md flex flex-col justify-center items-center border-t-4 border-yellow-500 p-6 hover:shadow-xl transition-all duration-200 cursor-pointer">
-                    <div className="text-5xl mb-2">🔔</div>
+                    <div className="text-5xl mb-2">✍️</div>
                     <div className="text-lg font-bold text-gray-800 text-center">Notificatoins</div>
                   </Link>
 

a2b61374da8e2c75787595529e424ebdc9b3822f
wirecoreltd <wirecoreltd@gmail.com>
2026-05-05 17:28:45 +0400

    Update notifications.js

--- pages/admin/notifications.js
+++ pages/admin/notifications.js
@@ -58,7 +58,7 @@ function EtatBadge({ etat }) {
 
 export default function NotificationsPage() {
   return (
-    <ProtectedRoute allowedRoles={["Administrateur", "Conseiller", "ResponsableFamilles", "ResponsableCellule", "ResponsableIntegration"]}>
+    <ProtectedRoute allowedRoles={["Administrateur", "Conseiller", "ResponsableFamilles", "ResponsableCellule", "ResponsableIntegration", "ResponsableEvangelisation"]}>
       <NotificationsContent />
     </ProtectedRoute>
   );

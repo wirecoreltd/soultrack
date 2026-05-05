ad1088cfc5ec64cddbd8455a75616a6555657519
wirecoreltd <wirecoreltd@gmail.com>
2026-05-05 09:59:31 +0400

    Update cellules-hub.js

--- pages/cellule/cellules-hub.js
+++ pages/cellule/cellules-hub.js
@@ -102,15 +102,17 @@ useEffect(() => {
             </div>
           </Link>
         
-          <Link
-            href="/cellule/ajouter-membre-cellule"
-            className="flex-1 min-w-[250px] w-full h-32 bg-white rounded-3xl shadow-md flex flex-col justify-center items-center border-t-4 border-blue-500 p-6 hover:shadow-xl transition-all duration-200 cursor-pointer"
-          >
-            <div className="text-5xl mb-2">➕</div>
-            <div className="text-lg font-bold text-gray-800 text-center">
-              Ajouter un membre à la Cellule
-            </div>
-          </Link>
+          {!loadingRole && role === "ResponsableCellule" && (
+            <Link
+              href="/cellule/ajouter-membre-cellule"
+              className="flex-1 min-w-[250px] w-full h-32 bg-white rounded-3xl shadow-md flex flex-col justify-center items-center border-t-4 border-blue-500 p-6 hover:shadow-xl transition-all duration-200 cursor-pointer"
+            >
+              <div className="text-5xl mb-2">➕</div>
+              <div className="text-lg font-bold text-gray-800 text-center">
+                Ajouter un membre à la Cellule
+              </div>
+            </Link>
+          )}
         
           <Link
             href="/cellule/membres-cellule"

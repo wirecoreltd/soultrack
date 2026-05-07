f94187d036c6cbb3764c3c825555be1609d685fe
wirecoreltd <wirecoreltd@gmail.com>
2026-05-07 12:41:47 +0400

    Update conseiller-hub.js

--- pages/conseiller/conseiller-hub.js
+++ pages/conseiller/conseiller-hub.js
@@ -165,7 +165,7 @@ function ConseillerHubContent() {
           className="flex-1 bg-white rounded-2xl shadow-md flex flex-col justify-center items-center border-t-4 border-yellow-500 p-6 hover:shadow-lg transition-all duration-200 cursor-pointer h-32"
         >
           <div className="text-4xl mb-2">✍🏻</div>
-          <div className="text-lg font-bold text-gray-800 text-center">Présence</div>
+          <div className="text-lg font-bold text-gray-800 text-center">Registre des présences</div>
         </Link>
       </div>
 

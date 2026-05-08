c384b71bd3b87d28025d91b334ea71fe86961822
wirecoreltd <wirecoreltd@gmail.com>
2026-05-08 11:50:43 +0400

    Update familles-hub.js

--- pages/famille/familles-hub.js
+++ pages/famille/familles-hub.js
@@ -189,14 +189,17 @@ function FamillesHubContent() {
           <div className="text-5xl mb-2">✍️</div>
           <div className="text-lg font-bold text-gray-800 text-center">
             Registre des présences
-
-            <Link
+          </div>
+        </Link>
+        
+        <Link
           href="/rapport/RapportPresence"
           className="flex-1 min-w-[250px] w-full h-32 bg-white rounded-3xl shadow-md flex flex-col justify-center items-center border-t-4 border-yellow-500 p-6 hover:shadow-xl transition-all duration-200 cursor-pointer"
         >
           <div className="text-5xl mb-2">✅</div>
           <div className="text-lg font-bold text-gray-800 text-center">
-            Rapport de Registre       
+            Rapport de Registre
+          </div>
         </Link>
         {NOTIF_CARD}
       </div>

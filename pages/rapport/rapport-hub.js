d794e93260933946811083c05ca0fa587fe6658a
wirecoreltd <wirecoreltd@gmail.com>
2026-05-13 15:52:55 +0400

    Update rapport-hub.js

--- pages/rapport/rapport-hub.js
+++ pages/rapport/rapport-hub.js
@@ -122,7 +122,16 @@ function RapportHubContent() {
           >
             <div className="text-4xl mb-2">✅</div>
             <div className="text-lg font-bold text-gray-800 text-center">Registres</div>
-          </Link>         
+          </Link>      
+
+              <Link
+            href="/rapport/StatGlobalPage"
+            className="flex-1 bg-white rounded-2xl shadow-md flex flex-col justify-center items-center 
+            border-t-4 border-[#F59E0B] p-6 hover:shadow-lg transition-all duration-200 cursor-pointer h-32"
+          >
+            <div className="text-4xl mb-2">🌍</div>
+            <div className="text-lg font-bold text-gray-800 text-center">Stats Globale</div>
+          </Link>    
 
         </div>
       </div>

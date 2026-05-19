f503c565b929def506c791774390d0517160b851
wirecoreltd <wirecoreltd@gmail.com>
2026-05-19 15:46:22 +0400

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,11 +1,14 @@
 import { MembersProvider } from "../context/MembersContext";
+import { FeaturesProvider } from "../components/FeaturesContext";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="fr">
       <body>
         <MembersProvider>
-          {children}
+          <FeaturesProvider>
+            {children}
+          </FeaturesProvider>
         </MembersProvider>
       </body>
     </html>

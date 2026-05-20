ac6c696b0f0ed99e0b65666f8d05efd28c54be46
wirecoreltd <wirecoreltd@gmail.com>
2026-05-20 22:46:30 +0400

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -1,21 +1,13 @@
 "use client";
-import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer className="w-full py-6 mt-auto">
-      <div className="max-w-5xl mx-auto text-center text-sm text-gray-400 space-y-2">
-
-        <div>
-          © {new Date().getFullYear()} SoulTrack. Tous droits réservés.
-        </div>
-
-        <div className="flex justify-center gap-4 text-blue-300 underline">
-          <Link href="/site/terms">Terms of Service</Link>
-          <Link href="/site/privacy">Privacy Policy</Link>
-          <Link href="/site/refund">Refund Policy</Link>
-        </div>
-
+    <footer className="w-full py-4 mt-auto">
+      <div
+        className="max-w-5xl mx-auto text-center text-sm"
+        style={{ color: "#808080" }}
+      >
+        © {new Date().getFullYear()} SoulTrack. Tous droits réservés.
       </div>
     </footer>
   );

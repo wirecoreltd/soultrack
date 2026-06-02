d48dc72037468f7c061140b2fab99dca15ac6852
wirecoreltd <wirecoreltd@gmail.com>
2026-06-02 09:13:34 +0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,16 +2,25 @@ import "../styles/globals.css";
 import { MembersProvider } from "../context/MembersContext";
 import { FeaturesProvider } from "../components/FeaturesContext";
 import { NotificationsProvider } from "../context/NotificationsContext";
+import { Great_Vibes } from "next/font/google";
+
+const greatVibes = Great_Vibes({
+  subsets: ["latin"],
+  weight: "400",
+  variable: "--font-great-vibes",
+});
 
 function MyApp({ Component, pageProps }) {
   return (
-    <FeaturesProvider>
-      <MembersProvider>
-        <NotificationsProvider>
-          <Component {...pageProps} />
-        </NotificationsProvider>
-      </MembersProvider>
-    </FeaturesProvider>
+    <div className={greatVibes.variable}>
+      <FeaturesProvider>
+        <MembersProvider>
+          <NotificationsProvider>
+            <Component {...pageProps} />
+          </NotificationsProvider>
+        </MembersProvider>
+      </FeaturesProvider>
+    </div>
   );
 }
 

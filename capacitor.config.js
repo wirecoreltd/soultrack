4794747c6995ed46b47b4dc3a99ca5f2200c58dc
wirecoreltd <wirecoreltd@gmail.com>
2026-06-03 23:14:39 +0400

    Update capacitor.config.js

--- capacitor.config.js
+++ capacitor.config.js
@@ -2,7 +2,7 @@ module.exports = {
   appId: 'org.soultrack.app',
   appName: 'SoulTrack',
   server: {
-    url: 'https://www.soultrack.org',
+    url: 'https://www.soultrack.org/login',
     cleartext: false,
   },
   plugins: {

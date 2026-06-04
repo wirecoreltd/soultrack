734d66cb77e8425c879f90ecaf731188fd0b1d62
wirecoreltd <wirecoreltd@gmail.com>
2026-06-04 09:10:09 +0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,11 @@
 import { useEffect } from "react";
 import { useRouter } from "next/router";
-import supabase from "../lib/supabaseClient";
 
 export default function Home() {
   const router = useRouter();
 
   useEffect(() => {
-    const check = async () => {
-      const { data } = await supabase.auth.getSession();
-      if (data?.session) {
-        router.replace("/hub");
-      } else {
-        router.replace("/login");
-      }
-    };
-    check();
+    router.replace("/login");
   }, [router]);
 
   return null;

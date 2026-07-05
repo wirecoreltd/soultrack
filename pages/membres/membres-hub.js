251af89fe1e8a2d0e354726de7ed9eb5df701390
wirecoreltd <wirecoreltd@gmail.com>
2026-07-05 19:44:53 +0400

    Update membres-hub.js

--- pages/membres/membres-hub.js
+++ pages/membres/membres-hub.js
@@ -139,7 +139,7 @@ function MembresHubContent() {
         </Link>
 
         <Link href="/rapport/attendance" className="bg-white rounded-2xl shadow-md flex flex-col justify-center items-center border-t-4 border-[#0EA5E9] p-6 hover:shadow-lg transition-all duration-200 cursor-pointer h-32">
-          <div className="text-4xl mb-2">🗒️</div>
+          <div className="text-4xl mb-2">🛐</div>
           <div className="text-lg font-bold text-gray-800 text-center">{t.cards.presencesRapports}</div>
         </Link>
 

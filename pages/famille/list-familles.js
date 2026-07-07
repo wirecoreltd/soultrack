58e5bd4cf0d5bdb32ed60013bf6b74e0020f4956
wirecoreltd <wirecoreltd@gmail.com>
2026-07-07 11:25:35 +0400

    Update list-familles.js

--- pages/famille/list-familles.js
+++ pages/famille/list-familles.js
@@ -430,7 +430,7 @@ function ListFamillesContent() {
       </div>
 
       {/* Bouton ajout */}
-      {(role === "Administrateur" || role === "SuperviseurFamille") && (
+      {(role === "Administrateur" || role === "SuperviseurFamilles") && (
         <div className="max-w-6xl mx-auto flex justify-end mb-3">
           <button
             onClick={() => router.push("/admin/create-famille")}

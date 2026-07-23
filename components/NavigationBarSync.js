a1460250c3e4494c9d8471787f6853e60165fc3f
wirecoreltd <wirecoreltd@gmail.com>
2026-07-23 11:52:12 +0400

    Update NavigationBarSync.js

--- components/NavigationBarSync.js
+++ components/NavigationBarSync.js
@@ -2,7 +2,7 @@
 
 import { useNavigationBarColor } from "../hooks/useNavigationBarColor";
 
-// Nav bar blanche partout, icônes foncées. Simple, fixe, fiable.
+// Nav bar blanche partout, icônes foncées.Simple, fixe,fiable.
 export default function NavigationBarSync() {
   useNavigationBarColor("#FFFFFF", true);
   return null;

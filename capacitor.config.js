8b5daf05e150284f9ad20001565598575726a98b
wirecoreltd <wirecoreltd@gmail.com>
2026-09-05 00:36:44 +0400

    Update capacitor.config.js

--- capacitor.config.js
+++ capacitor.config.js
@@ -2,9 +2,9 @@ module.exports = {
   appId: 'org.soultrack.app',
   appName: 'SoulTrack',
   server: {
-    url: 'https://soultrack.org/login',   // ← retirer le "www."
+    url: 'https://soultrack.org/login',   // ← sans "www."
     cleartext: false,
-    allowNavigation: ['soultrack.org', '*.soultrack.org'],  // ← filet de sécurité
+    allowNavigation: ['soultrack.org', '*.soultrack.org'],
   },
   android: {
     allowMixedContent: true,

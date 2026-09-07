c5616483a7f94dd0024b60e5fd3131495b86cdd9
wirecoreltd <wirecoreltd@gmail.com>
2026-09-07 14:12:49 +0400

    Update [tutorial].js

--- pages/aide/[slug]/[tutorial].js
+++ pages/aide/[slug]/[tutorial].js
@@ -1,8 +1,8 @@
 import { useRouter } from "next/router";
 import Head from "next/head";
-import { useLang } from "../../../hooks/useLang";
-import HeaderSite from "../../../components/HeaderSite";
-import { categories, tutorials } from "../../../lib/aideContent";
+import { useLang } from "../../hooks/useLang";
+import HeaderSite from "../../components/HeaderSite";
+import { categories, tutorials } from "../../lib/aideContent";
 
 const translations = {
   fr: {

f3259ab58118c79e84173848035529cad6012d4d
wirecoreltd <wirecoreltd@gmail.com>
2026-09-10 16:04:42 +0400

    Update hub.js

--- pages/hub.js
+++ pages/hub.js
@@ -203,7 +203,7 @@ export default function IndexPage() {
   return (
     <div
       className="min-h-screen flex flex-col items-center p-6 text-center space-y-6"
-      style={{ background: "linear-gradient(to bottom, #333699 0%, #3A45A0 10%, #4B5BB5 50%, #3A45A0 90%, #333699 100%)" }}
+      style={{ background: "linear-gradient(to bottom, #333699 0%, #363FA0 15%, #3D4DAA 35%, #4560B5 55%, #2E6DBF 100%)" }}
     >
       <HeaderPages />
 

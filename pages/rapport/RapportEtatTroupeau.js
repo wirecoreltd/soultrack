3d43b177cebcb41d72c7c5f8c98cc222a14dfa1e
wirecoreltd <wirecoreltd@gmail.com>
2026-09-11 11:53:28 +0400

    Update RapportEtatTroupeau.js

--- pages/rapport/RapportEtatTroupeau.js
+++ pages/rapport/RapportEtatTroupeau.js
@@ -91,7 +91,9 @@ function RapportFormation() {
 
   /* ================= RENDER ================= */
   return (
-    <div className="min-h-screen flex flex-col items-center p-6 bg-[#333699]">
+    <div className="min-h-screen flex flex-col items-center p-6" 
+    style={{ background: "linear-gradient(to bottom, #3A48A0 0%, #3A48A0 10%, #405BAF 30%, #3E7DCF 55%, #405BAF 80%, #3A48A0 100%)" }}
+>
       <HeaderPages />
 
       <h1 className="text-2xl font-bold mt-4 mb-6 text-center">

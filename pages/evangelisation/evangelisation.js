2406ca91d0ae342a7d9f46e0080ee5b621c4bb06
wirecoreltd <wirecoreltd@gmail.com>
2026-09-11 10:22:30 +0400

    Update background style of evangelisation page

--- pages/evangelisation/evangelisation.js
+++ pages/evangelisation/evangelisation.js
@@ -651,7 +651,8 @@ ${t.msgInfos} ${m.infos_supplementaires || "—"}
   };
 
   return (
-    <div className="min-h-screen flex flex-col items-center p-6 bg-[#333699]">
+    <div className="min-h-screen flex flex-col items-center p-6" 
+    style={{ background: "linear-gradient(to bottom, #3A48A0 0%, #3A48A0 10%, #405BAF 30%, #3E7DCF 55%, #405BAF 80%, #3A48A0 100%)" }}>
       <HeaderPages />
       <h1 className="text-2xl font-bold mt-4 mb-6 text-blue-300 text-center text-white">
         {t.titre1}<span className="text-emerald-300">{t.titre2}</span>
